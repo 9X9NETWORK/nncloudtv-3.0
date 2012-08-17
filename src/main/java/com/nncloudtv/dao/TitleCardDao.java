e58a6a47b37a773bc0679183f0c9ae557042bf8f
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2012-08-17 01:59:17 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/9x9dev/trunk/nncloudtv@3107 a0a5f996-2bb4-401c-8778-395c3c01700f

---
+++ src/main/java/com/nncloudtv/dao/TitleCardDao.java
@@ -0,0 +1,38 @@
+package com.nncloudtv.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+
+import com.nncloudtv.lib.PMF;
+import com.nncloudtv.model.TitleCard;
+
+public class TitleCardDao extends GenericDao<TitleCard> {
+	
+	protected static final Logger log = Logger.getLogger(TitleCardDao.class.getName());
+	
+	public TitleCardDao() {
+		super(TitleCard.class);
+	}	
+		
+	public List<TitleCard> findByChannel(long channelId) {
+		PersistenceManager pm = PMF.getContent().getPersistenceManager();
+		List<TitleCard> detached = new ArrayList<TitleCard>(); 
+		try {
+			Query q = pm.newQuery(TitleCard.class);
+			q.setFilter("channelId == channelIdParam");
+			q.declareParameters("long channelIdParam");
+			q.setOrdering("entryId");
+			@SuppressWarnings("unchecked")
+			List<TitleCard> cards = (List<TitleCard>) q.execute(channelId);
+			detached = (List<TitleCard>)pm.detachCopyAll(cards);
+		} finally {
+			pm.close();
+		}
+		return detached;
+	}	
+	
+}
