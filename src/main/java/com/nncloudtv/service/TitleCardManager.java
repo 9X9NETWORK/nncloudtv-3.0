a2375bcc491d47fd0504985f47f0b163df6e2cdc
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2012-08-17 23:40:00 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/9x9dev/trunk/nncloudtv@3118 a0a5f996-2bb4-401c-8778-395c3c01700f

---
+++ src/main/java/com/nncloudtv/service/TitleCardManager.java
@@ -0,0 +1,50 @@
+package com.nncloudtv.service;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.logging.Logger;
+
+import org.springframework.stereotype.Service;
+
+import com.nncloudtv.dao.TitleCardDao;
+import com.nncloudtv.model.TitleCard;
+
+@Service
+public class TitleCardManager {
+
+	protected static final Logger log = Logger.getLogger(TitleCardManager.class.getName());
+	
+	private TitleCardDao dao = new TitleCardDao();
+	
+	public TitleCard save(TitleCard card) {
+		card.setPlayerSyntax(this.generatePlayerSyntax(card));
+		dao.save(card);
+		return card;
+	}
+	
+	
+	private String generatePlayerSyntax(TitleCard card) {
+		if (card == null) return null;
+		if (card.getSubSeq() == null || card.getMessage() == null) 
+			return null;
+		String syntax = "";
+		syntax += "subepisode: " + Long.parseLong(card.getSubSeq());
+		syntax += "message: " + card.getSubSeq();
+		if (card.getDuration() != null)
+			syntax += "duration: " + card.getDuration();
+		if (card.getStyle() != null)
+			syntax += "style: " + card.getStyle();
+		if (card.getColor() != null)
+			syntax += "color: " + card.getColor();
+		if (card.getBgcolor() != null)
+			syntax += "bgcolor: " + card.getColor();		
+		try {
+			syntax = URLEncoder.encode(syntax, "UTF-8");
+			log.info("syntax:" + syntax);
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return syntax;
+	}
+}
