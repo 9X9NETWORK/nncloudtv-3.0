f5781d91ca011ce0fc6fcdc43b1af747bdca351f
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2012-09-05 00:01:24 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/9x9dev/trunk/nncloudtv@3171 a0a5f996-2bb4-401c-8778-395c3c01700f

---
+++ src/main/java/com/nncloudtv/model/NnAd.java
@@ -0,0 +1,94 @@
+package com.nncloudtv.model;
+
+import java.util.Date;
+
+import javax.jdo.annotations.Column;
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable(table="nn_ad", detachable="true")
+public class NnAd {
+	@PrimaryKey
+	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	private long id;
+
+	@Persistent
+	private long programId;
+	
+	@Persistent
+	@Column(jdbcType="VARCHAR", length=255)
+	private String merchantEmail;
+	
+	@Persistent
+	@Column(jdbcType="VARCHAR", length=255)
+	private String message;
+	
+	@Persistent
+	@Column(jdbcType="VARCHAR", length=255)
+	private String url;
+	
+	@Persistent
+	private Date createDate;
+	
+	@Persistent
+	private Date updateDate;
+	
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public long getProgramId() {
+		return programId;
+	}
+
+	public void setProgramId(long programId) {
+		this.programId = programId;
+	}
+
+	public String getMerchantEmail() {
+		return merchantEmail;
+	}
+
+	public void setMerchantEmail(String merchantEmail) {
+		this.merchantEmail = merchantEmail;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+}
---
+++ src/main/java/com/nncloudtv/model/NnUserLibrary.java
@@ -0,0 +1,95 @@
+package com.nncloudtv.model;
+
+import java.util.Date;
+
+import javax.jdo.annotations.Column;
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable(table="nnuser_library", detachable="true")
+public class NnUserLibrary {
+	@PrimaryKey
+	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	private long id;
+ 
+	@Persistent
+	@Column(jdbcType="VARCHAR", length=25)	
+	private String userIdStr; //format: shard-userId, example: 1-1
+
+	@Persistent
+	@Column(jdbcType="VARCHAR", length=500)
+	private String name;
+
+	@Persistent
+	@Column(jdbcType="VARCHAR", length=500)
+	private String fileUrl; 
+		
+	@Persistent
+	@Column(jdbcType="VARCHAR", length=255)
+	private String imageUrl;
+	
+	@Persistent
+	private short status; //correspond to NnProgram status
+		
+	@Persistent
+	private Date updateDate;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUserIdStr() {
+		return userIdStr;
+	}
+
+	public void setUserIdStr(String userIdStr) {
+		this.userIdStr = userIdStr;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFileUrl() {
+		return fileUrl;
+	}
+
+	public void setFileUrl(String fileUrl) {
+		this.fileUrl = fileUrl;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public short getStatus() {
+		return status;
+	}
+
+	public void setStatus(short status) {
+		this.status = status;
+	}
+
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+	
+}
