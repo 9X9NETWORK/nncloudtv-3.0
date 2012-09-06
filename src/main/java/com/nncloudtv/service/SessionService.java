d5bf7ef5502b1fe37c6d497d18c1ec15f3e6cc47
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2012-09-06 20:24:52 +0000

    replace tab with spaces
    git-svn-id: https://everest.teltel.com:8443/repos/misc/9x9dev/trunk/nncloudtv@3186 a0a5f996-2bb4-401c-8778-395c3c01700f

--- src/main/java/com/nncloudtv/service/SessionService.java
+++ src/main/java/com/nncloudtv/service/SessionService.java
@@ -18,58 +18,58 @@ import com.nncloudtv.lib.CookieHelper;
 @Service
 // for stateless, we save session in cache now, or other media later
 public class SessionService {
-	
-	protected static final Logger log = Logger.getLogger(SessionService.class.getName());
-	
-	private HttpSession session;
-	
-	public SessionService() {
-		session = null;
-	}
-	
-	public SessionService(HttpServletRequest request) {		
-		session = request.getSession();
-		String sessionId = CookieHelper.getCookie(request, CookieHelper.CMS_SESSION);
-		if (sessionId == null) {
-			sessionId = session.getId();
-		}
-		log.info("session id = " + sessionId);
-		
-		@SuppressWarnings("unchecked")
-		Map<String, Object> map = (Map<String, Object>) CacheFactory.get(sessionId); 
-		if (map != null) {
-			Iterator<String> iterator = map.keySet().iterator();
-			while (iterator.hasNext()) {
-				String name = iterator.next();
-				log.info("cached session = " + name);
-				session.setAttribute(name, map.get(name));
-			}
-		}
-	}
-	
-	public void saveSession(HttpSession session, HttpServletResponse resp) {
-		this.session = session;
-		saveSession(resp);
-	}
-	
-	public void saveSession(HttpServletResponse resp) {
-		HashMap<String, Object> map = new HashMap<String, Object>();
-		for (@SuppressWarnings("unchecked") Enumeration<String> enu = (Enumeration<String>)session.getAttributeNames(); enu.hasMoreElements();) {
-			String name = (String)enu.nextElement();
-			log.info("put to cache = " + name);
-			map.put(name, session.getAttribute(name));
-		}
-		CacheFactory.set(session.getId(), map);
-		CookieHelper.setCookie(resp, CookieHelper.CMS_SESSION, session.getId());
-	}
-	
-	public void removeSession(HttpServletResponse resp) {
-		CacheFactory.delete(session.getId());
-		CookieHelper.deleteCookie(resp, CookieHelper.CMS_SESSION);
-		session.invalidate();
-	}
-	
-	public HttpSession getSession() {
-		return session;
-	}
+    
+    protected static final Logger log = Logger.getLogger(SessionService.class.getName());
+    
+    private HttpSession session;
+    
+    public SessionService() {
+        session = null;
+    }
+    
+    public SessionService(HttpServletRequest request) {        
+        session = request.getSession();
+        String sessionId = CookieHelper.getCookie(request, CookieHelper.CMS_SESSION);
+        if (sessionId == null) {
+            sessionId = session.getId();
+        }
+        log.info("session id = " + sessionId);
+        
+        @SuppressWarnings("unchecked")
+        Map<String, Object> map = (Map<String, Object>) CacheFactory.get(sessionId); 
+        if (map != null) {
+            Iterator<String> iterator = map.keySet().iterator();
+            while (iterator.hasNext()) {
+                String name = iterator.next();
+                log.info("cached session = " + name);
+                session.setAttribute(name, map.get(name));
+            }
+        }
+    }
+    
+    public void saveSession(HttpSession session, HttpServletResponse resp) {
+        this.session = session;
+        saveSession(resp);
+    }
+    
+    public void saveSession(HttpServletResponse resp) {
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        for (@SuppressWarnings("unchecked") Enumeration<String> enu = (Enumeration<String>)session.getAttributeNames(); enu.hasMoreElements();) {
+            String name = (String)enu.nextElement();
+            log.info("put to cache = " + name);
+            map.put(name, session.getAttribute(name));
+        }
+        CacheFactory.set(session.getId(), map);
+        CookieHelper.setCookie(resp, CookieHelper.CMS_SESSION, session.getId());
+    }
+    
+    public void removeSession(HttpServletResponse resp) {
+        CacheFactory.delete(session.getId());
+        CookieHelper.deleteCookie(resp, CookieHelper.CMS_SESSION);
+        session.invalidate();
+    }
+    
+    public HttpSession getSession() {
+        return session;
+    }
 }
--- src/main/java/com/nncloudtv/service/TitleCardManager.java
+++ src/main/java/com/nncloudtv/service/TitleCardManager.java
@@ -12,39 +12,39 @@ import com.nncloudtv.model.TitleCard;
 @Service
 public class TitleCardManager {
 
-	protected static final Logger log = Logger.getLogger(TitleCardManager.class.getName());
-	
-	private TitleCardDao dao = new TitleCardDao();
-	
-	public TitleCard save(TitleCard card) {
-		card.setPlayerSyntax(this.generatePlayerSyntax(card));
-		dao.save(card);
-		return card;
-	}
-	
-	
-	private String generatePlayerSyntax(TitleCard card) {
-		if (card == null) return null;
-		if (card.getSubSeq() == null || card.getMessage() == null) 
-			return null;
-		String syntax = "";
-		syntax += "subepisode: " + Long.parseLong(card.getSubSeq());
-		syntax += "message: " + card.getSubSeq();
-		if (card.getDuration() != null)
-			syntax += "duration: " + card.getDuration();
-		if (card.getStyle() != null)
-			syntax += "style: " + card.getStyle();
-		if (card.getColor() != null)
-			syntax += "color: " + card.getColor();
-		if (card.getBgColor() != null)
-			syntax += "bgcolor: " + card.getColor();		
-		try {
-			syntax = URLEncoder.encode(syntax, "UTF-8");
-			log.info("syntax:" + syntax);
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-			return null;
-		}
-		return syntax;
-	}
+    protected static final Logger log = Logger.getLogger(TitleCardManager.class.getName());
+    
+    private TitleCardDao dao = new TitleCardDao();
+    
+    public TitleCard save(TitleCard card) {
+        card.setPlayerSyntax(this.generatePlayerSyntax(card));
+        dao.save(card);
+        return card;
+    }
+    
+    
+    private String generatePlayerSyntax(TitleCard card) {
+        if (card == null) return null;
+        if (card.getSubSeq() == null || card.getMessage() == null) 
+            return null;
+        String syntax = "";
+        syntax += "subepisode: " + Long.parseLong(card.getSubSeq());
+        syntax += "message: " + card.getSubSeq();
+        if (card.getDuration() != null)
+            syntax += "duration: " + card.getDuration();
+        if (card.getStyle() != null)
+            syntax += "style: " + card.getStyle();
+        if (card.getColor() != null)
+            syntax += "color: " + card.getColor();
+        if (card.getBgColor() != null)
+            syntax += "bgcolor: " + card.getColor();        
+        try {
+            syntax = URLEncoder.encode(syntax, "UTF-8");
+            log.info("syntax:" + syntax);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return syntax;
+    }
 }
