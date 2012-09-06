2467955b416289eef9bfbb52df9d11528a19ead9
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2012-09-06 20:18:29 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/9x9dev/trunk/nncloudtv@3180 a0a5f996-2bb4-401c-8778-395c3c01700f

---
+++ src/main/java/com/nncloudtv/service/TagManager.java
@@ -0,0 +1,34 @@
+package com.nncloudtv.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.springframework.stereotype.Service;
+
+import com.nncloudtv.dao.TagDao;
+import com.nncloudtv.model.NnChannel;
+import com.nncloudtv.model.Tag;
+import com.nncloudtv.model.TagMap;
+
+@Service
+public class TagManager {
+
+    protected static final Logger log = Logger.getLogger(TagManager.class.getName());
+    private TagDao dao = new TagDao();
+    
+    public List<NnChannel> findChannelsByTag(String name, boolean player) {        
+        Tag tag = dao.findByName(name);
+        List<NnChannel> channels = new ArrayList<NnChannel>();
+        NnChannelManager chMngr = new NnChannelManager();
+        if (tag != null) {            
+            List<TagMap> map = dao.findMap(tag.getId());
+            for (TagMap m : map) {
+                NnChannel c = chMngr.findById(m.getChannelId());
+                if (c != null)
+                    channels.add(c);
+            }
+        }        
+        return channels;
+    }        
+}
