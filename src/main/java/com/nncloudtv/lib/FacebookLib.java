d7cbb7c4baf2ba833b3bbb557ffa591bd27145fd
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2012-09-06 20:28:23 +0000

    server auth
    git-svn-id: https://everest.teltel.com:8443/repos/misc/9x9dev/trunk/nncloudtv@3196 a0a5f996-2bb4-401c-8778-395c3c01700f

--- src/main/java/com/nncloudtv/lib/FacebookLib.java
+++ src/main/java/com/nncloudtv/lib/FacebookLib.java
@@ -7,11 +7,14 @@ import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Logger;
 
+import org.apache.commons.lang.RandomStringUtils;
 import org.codehaus.jackson.map.ObjectMapper;
 
 import twitter4j.Twitter;
@@ -27,153 +30,248 @@ import com.nncloudtv.web.json.facebook.FacebookPage;
 import com.nncloudtv.web.json.facebook.FacebookResponse;
 
 public class FacebookLib {
-	protected static final Logger log = Logger.getLogger(FacebookLib.class.getName());
+    protected static final Logger log = Logger.getLogger(FacebookLib.class.getName());
 
-	//!!! rewrite
-	public String[] getFanpageInfo(String urlStr) {
-		String query = urlStr.replace("www.facebook.com", "graph.facebook.com");
-		query = query.replace("https", "http");
+    protected static String clientId = "417419178315486";
+    protected static String secret = "bb96e3578cfb0822796810601d554e97";
+    protected static String redirectUri = "http://dev1.9x9.tv/fb/login";
+    
+    private static String generateState() {
+        String time = String.valueOf(new Date().getTime());
+        String random = RandomStringUtils.randomAlphabetic(10);
+        String result = time + random;
+        result = RandomStringUtils.random(10, 0, 10, true, true, result.toCharArray());     
+        return result;
+    }
+    
+    //TODO change to json object
+    public String[] getFbMe(String accessToken) {
+        log.info("-- query facebook me --");
+        String[] data = {null, null, null, null, null, null};
+        try {
+            URL url = new URL("https://graph.facebook.com/me?access_token=" + URLEncoder.encode(accessToken, "utf-8"));
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestMethod("GET");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line = reader.readLine();
+            reader.close();
+            log.info(line);
+            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                data[0] = line.split("\"id\":\"")[1].split(",")[0].replace("\"", "");
+                String email = line.split("\"email\":\"")[1].split(",")[0].replace("\"", "").replace("\\u0040", "@");
+                data[1] = email;
+                data[2] = line.split("\"name\":\"")[1].split(",")[0].replace("\"", "");
+                data[3] = line.split("\"gender\":\"")[1].split(",")[0].replace("\"", "");
+                data[4] = line.split("\"locale\":\"")[1].split(",")[0].replace("\"", "");
+                data[5] = line.split("\"birthday\":\"")[1].split(",")[0].replace("\"", "");
+            } else {
+                log.info("response status:" + connection.getResponseCode() + ";" + connection.getResponseMessage());                
+            }
+        } catch (MalformedURLException e) {
+                e.printStackTrace();
+        } catch (ProtocolException e) {
+                e.printStackTrace();
+        } catch (IOException e) {
+                e.printStackTrace();
+        }
+        return data;
+    }
+    
+    public String[] getOAuthAccessToken(String code){
+        log.info("---- getOAuthAccessToken ----");
+        String urlBase = "https://graph.facebook.com/oauth/access_token";
+        String data[] = {null, null}; //token, expires
+        try {
+            URL url = new URL(urlBase);
+            String params = "client_id=" + clientId +             
+                            "&code=" + code + 
+                            "&client_secret=" + secret +
+                            "&redirect_uri=" + redirectUri;                 
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestMethod("POST");
+            OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+            writer.write(params);
+            writer.close(); 
+            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line = reader.readLine();
+                reader.close();
+                log.info(line);
+                data[0] = line.split("access_token=")[1].split("&expires=")[0];
+                if (line.split("expires=").length > 0)
+                    data[1] = line.split("expires=")[1];
+                log.info("token:" + data[0]);
+                log.info("expires:" + data[1]);
+            } else {
+                log.info("response status:" + connection.getResponseCode());
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+    
+    public static String getDialogOAuthPath() {        
+        String scope = "user_likes,user_location,user_interests,email,user_birthday";
+        String state = FacebookLib.generateState();
+        String urlBase = "http://www.facebook.com/dialog/oauth?";
+        String url = urlBase +
+                     "client_id=" + clientId +
+                     "&redirect_uri=" + redirectUri +
+                     "&scope=" + scope +
+                     "&state=" + state;
+        log.info("oauth dialog:" + url);
+        return url;
+    }
+
+    public String[] getFanpageInfo(String urlStr) {
+        String query = urlStr.replace("www.facebook.com", "graph.facebook.com");
+        query = query.replace("https", "http");
         URL url;
         String fbInfo[] = new String[2];
-    	String username = "";
-    	String picture = "";
-    	if (urlStr.contains("pages")) {
-	    	int start = urlStr.indexOf("pages/", 1);
-	    	query = urlStr.substring(start+6);
-	    	start = query.indexOf("/", 1);
-	    	if (start > 1)
-	    	   query = query.substring(start+1);
-	    	   query = "http://graph.facebook.com/" + query;
-	    }
+        String username = "";
+        String picture = "";
+        if (urlStr.contains("pages")) {
+            int start = urlStr.indexOf("pages/", 1);
+            query = urlStr.substring(start+6);
+            start = query.indexOf("/", 1);
+            if (start > 1)
+               query = query.substring(start+1);
+               query = "http://graph.facebook.com/" + query;
+        }
         try {
-			//HTTP GET
-			url = new URL(query);						
-	        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-	        connection.setDoOutput(true);
-			connection.setRequestProperty("Content-Type", "application/json");
-	        
-	        //connection.setDoOutput(true);
-	        if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-	        	log.info("podcast GET response not ok!" + connection.getResponseCode());	        	
-	        }
-	        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+            //HTTP GET
+            url = new URL(query);                       
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestProperty("Content-Type", "application/json");
+            
+            //connection.setDoOutput(true);
+            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                log.info("podcast GET response not ok!" + connection.getResponseCode());                
+            }
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
             String inputLine;
 
             while ((inputLine = reader.readLine()) != null) {
-            	String[] line = inputLine.split(",");
-            	for (String l : line) {
-            		if (l.contains("name") || l.contains("picture")) {
-            			l = l.replaceAll("\"", "");
-            			String[] entity = l.split(":");            			
-            			if (l.contains("name")) 
-            				username = entity[1];
-            			if (l.contains("picture")) 
-            				picture = entity[1] + entity[2];
-            			    picture = picture.replace("http\\/\\/", "http://");
-            			    picture = picture.replace("\\/", "/");
-            		}            				
-            	}
+                String[] line = inputLine.split(",");
+                for (String l : line) {
+                    if (l.contains("name") || l.contains("picture")) {
+                        l = l.replaceAll("\"", "");
+                        String[] entity = l.split(":");                     
+                        if (l.contains("name")) 
+                            username = entity[1];
+                        if (l.contains("picture")) 
+                            picture = entity[1] + entity[2];
+                            picture = picture.replace("http\\/\\/", "http://");
+                            picture = picture.replace("\\/", "/");
+                    }                           
+                }
             }
             reader.close();            
-	        fbInfo[0] = username;
-	        fbInfo[1] = picture; 
-		} catch (Exception e) {
-			fbInfo[0] = "";
-		}
-		return fbInfo;
-	}
-	
-	public static void postToFacebook(FBPost fbPost) throws IOException {
-		
-		MsoConfigManager configMngr = new MsoConfigManager();
-		MsoConfig fbConfig = configMngr.findByItem(MsoConfig.REALFBTOKEN);
-		String accessToken = null;
-		if (fbConfig != null) {
-			accessToken = fbConfig.getValue();
-		}
-		if (fbPost.getAccessToken() != null) {
-			accessToken = fbPost.getAccessToken();
-		}
-		
-		URL url = new URL("https://graph.facebook.com/" + fbPost.getFacebookId() + "/feed");
-		String post =
-			"access_token=" + URLEncoder.encode(accessToken, "US-ASCII") +
-			"&picture=" + URLEncoder.encode(fbPost.getPicture(), "US-ASCII") +
-			"&name=" + URLEncoder.encode(fbPost.getName(), "UTF-8") +
-			"&link=" + URLEncoder.encode(fbPost.getLink(), "US-ASCII")+
-			"&caption=" + URLEncoder.encode(fbPost.getCaption(), "UTF-8") +
-			"&description=" + URLEncoder.encode(fbPost.getDescription(), "UTF-8");
-		if (fbPost.getMessage() != null && !fbPost.getMessage().isEmpty()) {
-			post += "&message=" + URLEncoder.encode(fbPost.getMessage(), "UTF-8");
-		}
-		log.info(post);
-		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-		connection.setDoOutput(true);
-		connection.setRequestMethod("POST");
-		
-		OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
-		writer.write(post);
-		writer.close();
-		
-		BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-		String line = reader.readLine();
-		reader.close();
-		log.info(line);
-	}
-	
-	public static void postToTwitter(FBPost fbPost) throws IOException, TwitterException {
-		Twitter twitter = new TwitterFactory().getInstance();
-		twitter.setOAuthConsumer("udWzz6YrsaNlbJ18vZ7aCA", "Pf0TdB2QFXWKyphbIdnPG4vZhLVze0cPCxlLkfBwtQ");
-		AccessToken accessToken = new AccessToken(fbPost.getFacebookId(), fbPost.getAccessToken());
-		twitter.setOAuthAccessToken(accessToken);
-		twitter.updateStatus(fbPost.getName()+" "+fbPost.getLink());
-	}
-	
-	
-	static public List<FacebookPage> populatePageList(String fbUserId, String accessToken) {
-		
-		log.info("fbUserId = " + fbUserId + ", accessToken = " + accessToken);
-		
-		if (fbUserId == null || accessToken == null) {
-			return null;
-		}
-		
-		List<FacebookPage> pages = null;
-		
-		try {
-			String fullpath = 
-					"https://graph.facebook.com/" + fbUserId + 
-					"/accounts?access_token=" + URLEncoder.encode(accessToken, "US-ASCII");
-			log.info(fullpath);
-			URL url = new URL(fullpath);
-			
-			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-			
-			ObjectMapper mapper = new ObjectMapper();
-			FacebookResponse response = mapper.readValue(connection.getInputStream(), FacebookResponse.class);
-			if (response.getData() != null) {
-				pages = response.getData();
-				log.info("pages count: " + pages.size());
-			} else if (response.getError() != null) {
-				FacebookError error = response.getError();
-				log.warning("error message: " + error.getMessage());
-				log.warning("error type:" + error.getType());
-			} else {
-				log.warning("neither no data nor error");
-			}
-		} catch (MalformedURLException e) {
-			logException(e);
-		} catch (UnsupportedEncodingException e) {
-			logException(e);
-		} catch (IOException e) {
-			logException(e);
-		}
-		
-		return pages;
-	}
-	
-	static private void logException(Exception e) {
-		log.warning(e.getClass().getCanonicalName());
-		NnLogUtil.logException(e);
-	}
+            fbInfo[0] = username;
+            fbInfo[1] = picture; 
+        } catch (Exception e) {
+            fbInfo[0] = "";
+        }
+        return fbInfo;
+    }
+    
+    public static void postToFacebook(FBPost fbPost) throws IOException {
+        
+        MsoConfigManager configMngr = new MsoConfigManager();
+        MsoConfig fbConfig = configMngr.findByItem(MsoConfig.REALFBTOKEN);
+        String accessToken = null;
+        if (fbConfig != null) {
+            accessToken = fbConfig.getValue();
+        }
+        if (fbPost.getAccessToken() != null) {
+            accessToken = fbPost.getAccessToken();
+        }
+        
+        URL url = new URL("https://graph.facebook.com/" + fbPost.getFacebookId() + "/feed");
+        String post =
+            "access_token=" + URLEncoder.encode(accessToken, "US-ASCII") +
+            "&picture=" + URLEncoder.encode(fbPost.getPicture(), "US-ASCII") +
+            "&name=" + URLEncoder.encode(fbPost.getName(), "UTF-8") +
+            "&link=" + URLEncoder.encode(fbPost.getLink(), "US-ASCII")+
+            "&caption=" + URLEncoder.encode(fbPost.getCaption(), "UTF-8") +
+            "&description=" + URLEncoder.encode(fbPost.getDescription(), "UTF-8");
+        if (fbPost.getMessage() != null && !fbPost.getMessage().isEmpty()) {
+            post += "&message=" + URLEncoder.encode(fbPost.getMessage(), "UTF-8");
+        }
+        log.info(post);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setDoOutput(true);
+        connection.setRequestMethod("POST");
+        
+        OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+        writer.write(post);
+        writer.close();
+        
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String line = reader.readLine();
+        reader.close();
+        log.info(line);
+    }
+    
+    public static void postToTwitter(FBPost fbPost) throws IOException, TwitterException {
+        Twitter twitter = new TwitterFactory().getInstance();
+        twitter.setOAuthConsumer("udWzz6YrsaNlbJ18vZ7aCA", "Pf0TdB2QFXWKyphbIdnPG4vZhLVze0cPCxlLkfBwtQ");
+        AccessToken accessToken = new AccessToken(fbPost.getFacebookId(), fbPost.getAccessToken());
+        twitter.setOAuthAccessToken(accessToken);
+        twitter.updateStatus(fbPost.getName()+" "+fbPost.getLink());
+    }
+    
+    
+    static public List<FacebookPage> populatePageList(String fbUserId, String accessToken) {
+        
+        log.info("fbUserId = " + fbUserId + ", accessToken = " + accessToken);
+        
+        if (fbUserId == null || accessToken == null) {
+            return null;
+        }
+        
+        List<FacebookPage> pages = null;
+        
+        try {
+            String fullpath = 
+                    "https://graph.facebook.com/" + fbUserId + 
+                    "/accounts?access_token=" + URLEncoder.encode(accessToken, "US-ASCII");
+            log.info(fullpath);
+            URL url = new URL(fullpath);
+            
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            
+            ObjectMapper mapper = new ObjectMapper();
+            FacebookResponse response = mapper.readValue(connection.getInputStream(), FacebookResponse.class);
+            if (response.getData() != null) {
+                pages = response.getData();
+                log.info("pages count: " + pages.size());
+            } else if (response.getError() != null) {
+                FacebookError error = response.getError();
+                log.warning("error message: " + error.getMessage());
+                log.warning("error type:" + error.getType());
+            } else {
+                log.warning("neither no data nor error");
+            }
+        } catch (MalformedURLException e) {
+            logException(e);
+        } catch (UnsupportedEncodingException e) {
+            logException(e);
+        } catch (IOException e) {
+            logException(e);
+        }
+        
+        return pages;
+    }
+    
+    static private void logException(Exception e) {
+        log.warning(e.getClass().getCanonicalName());
+        NnLogUtil.logException(e);
+    }
 }
