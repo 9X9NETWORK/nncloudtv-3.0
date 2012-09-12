bf297578419c545635df2e81549d37d9204a17ed
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2012-09-12 19:48:55 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/9x9dev/trunk/nncloudtv@3284 a0a5f996-2bb4-401c-8778-395c3c01700f

---
+++ src/main/java/com/nncloudtv/service/NnEpisodeManager.java
@@ -0,0 +1,18 @@
+package com.nncloudtv.service;
+
+import java.util.logging.Logger;
+
+import com.nncloudtv.dao.NnEpisodeDao;
+import com.nncloudtv.model.NnEpisode;
+
+public class NnEpisodeManager{
+    
+    protected static final Logger log = Logger.getLogger(NnEpisodeManager.class.getName());
+    
+    private NnEpisodeDao dao = new NnEpisodeDao();
+    
+    public NnEpisode findById(long id) {
+        return dao.findById(id);
+    }
+        
+}
