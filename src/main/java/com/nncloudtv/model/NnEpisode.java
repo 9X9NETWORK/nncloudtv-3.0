c3e0cd3ea566e84e84656e6c82e030d8c49dcf6b
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2012-09-12 19:48:39 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/9x9dev/trunk/nncloudtv@3283 a0a5f996-2bb4-401c-8778-395c3c01700f

---
+++ src/main/java/com/nncloudtv/model/NnEpisode.java
@@ -0,0 +1,115 @@
+package com.nncloudtv.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.jdo.annotations.Column;
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable(table="nnepisode", detachable="true")
+public class NnEpisode implements Serializable {
+    private static final long serialVersionUID = -2365225197711392350L;
+
+    @PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+    private long id;
+
+    @Persistent
+    private long channelId;   
+    
+    @Persistent
+    @Column(jdbcType="VARCHAR", length=255)
+    private String name;
+
+    @Persistent
+    @Column(jdbcType="VARCHAR", length=255)
+    private String imageUrl;
+    
+    @Persistent
+    @Column(jdbcType="VARCHAR", length=255)
+    private String intro;
+
+    @Persistent
+    private long adId;   
+    
+    @Persistent
+    private Date publishDate;
+    
+    @Persistent
+    private Date updateDate;
+
+    public NnEpisode(long channelId, long programId) {
+        this.channelId = channelId;
+        Date now = new Date();
+        this.updateDate = now;
+        this.publishDate = now;
+    } 
+    
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getIntro() {
+        return intro;
+    }
+
+    public void setIntro(String intro) {
+        this.intro = intro;
+    }
+
+    public long getAdId() {
+        return adId;
+    }
+
+    public void setAdId(long adId) {
+        this.adId = adId;
+    }
+
+    public Date getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(Date publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public long getChannelId() {
+        return channelId;
+    }
+
+    public void setChannelId(long channelId) {
+        this.channelId = channelId;
+    }
+    
+}
