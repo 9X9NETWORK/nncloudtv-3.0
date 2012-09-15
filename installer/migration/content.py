27762f9347f2694dbcd255f4a149b55bda1578b8
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2012-09-15 06:40:51 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/9x9dev/trunk/nncloudtv@3331 a0a5f996-2bb4-401c-8778-395c3c01700f

--- installer/migration/content.py
+++ installer/migration/content.py
@@ -14,37 +14,53 @@ dbcontent = MySQLdb.connect (host = "localhost",
 contentCursor = dbcontent.cursor()
 contentCursor.execute("""truncate tag """)
 contentCursor.execute("""truncate tag_map """)
-
 contentCursor.execute("""insert into tag (name, updateDate) values ('hot(9x9en)', now()); """)
-contentCursor.execute("""insert into tag_map (channelId, tagId) values (1, 1); """)
-contentCursor.execute("""insert into tag_map (channelId, tagId) values (1, 2); """)
-contentCursor.execute("""insert into tag_map (channelId, tagId) values (1, 3); """)
-contentCursor.execute("""insert into tag_map (channelId, tagId) values (1, 4); """)
-contentCursor.execute("""insert into tag_map (channelId, tagId) values (1, 5); """)
-contentCursor.execute("""insert into tag_map (channelId, tagId) values (1, 6); """)
-contentCursor.execute("""insert into tag_map (channelId, tagId) values (1, 7); """)
-contentCursor.execute("""insert into tag_map (channelId, tagId) values (1, 8); """)
-contentCursor.execute("""insert into tag_map (channelId, tagId) values (1, 9); """)
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (1, 1); """)
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (1, 2); """)
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (1, 3); """)
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (1, 4); """)
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (1, 5); """)
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (1, 6); """)
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (1, 7); """)
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (1, 8); """)
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (1, 9); """)                                           
 contentCursor.execute("""insert into tag (name, updateDate) values ('hot(9x9zh)', now()); """)  
-contentCursor.execute("""insert into tag_map (channelId, tagId) values (2, 1); """)
-contentCursor.execute("""insert into tag_map (channelId, tagId) values (2, 2); """)
-contentCursor.execute("""insert into tag_map (channelId, tagId) values (2, 3); """)
-contentCursor.execute("""insert into tag_map (channelId, tagId) values (2, 4); """)
-contentCursor.execute("""insert into tag_map (channelId, tagId) values (2, 5); """)
-contentCursor.execute("""insert into tag_map (channelId, tagId) values (2, 6); """)
-contentCursor.execute("""insert into tag_map (channelId, tagId) values (2, 7); """)
-contentCursor.execute("""insert into tag_map (channelId, tagId) values (2, 8); """)
-contentCursor.execute("""insert into tag_map (channelId, tagId) values (2, 9); """)
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (2, 10); """)
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (2, 11); """)
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (2, 12); """)
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (2, 13); """)
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (2, 14); """)
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (2, 15); """)
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (2, 16); """)
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (2, 17); """)
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (2, 18); """)
 contentCursor.execute("""insert into tag (name, updateDate) values ('trending(9x9en)', now()); """) 
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (3, 19); """)
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (3, 20); """)
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (3, 21); """)
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (3, 22); """)
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (3, 23); """)
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (3, 24); """)
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (3, 25); """)
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (3, 26); """)
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (3, 27); """)
 contentCursor.execute("""insert into tag (name, updateDate) values ('trending(9x9zh)', now()); """) 
-                                                                                               
-                                                                                               
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (4, 28); """)
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (4, 29); """)
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (4, 30); """)
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (4, 31); """)
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (4, 32); """)
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (4, 33); """)
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (4, 34); """)
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (4, 35); """)
+contentCursor.execute("""insert into tag_map (tagId, channelId) values (4, 36); """)
+
 feed = open("contentPool.txt", "rU")                                                            
 i = 0
 for line in feed:
   i = i+1
   data = line.split('\t')
-  cId = data[0].rstrip().lstrip()
+  cId = data[0].rstrip().lstrip()   
   cName = data[1].rstrip().lstrip()
   categoryName = data[3].rstrip().lstrip()
   tags = data[4].rstrip("\"").lstrip("\"")
@@ -61,14 +77,22 @@ for line in feed:
      sphere = 'zh'
   if sphere == 'worldwide':
      sphere = 'other'
-  print "i: " + str(i) + ";cid: " + str(cId) + "; name:" + cName + "; categoryName: " + categoryName + "; lang:" + lang + "; sphere:" + sphere
-  ### update channel metadata
-  contentCursor.execute ("""  
+  print "i: " + str(i) + ";cid: " + str(cId) + "; name:" + cName + "; categoryName: " + categoryName + "; lang:" + lang + "; sphere:" + sphere  
+  contentCursor.execute ("""             
+     select id from nnchannel 
+      where id = %s 
+       """, (cId))        
+  cnt = contentCursor.rowcount
+  if cnt == 0:
+     print "this cid does not exist:" + str(cId)
+
+  ### update channel metadata               
+  contentCursor.execute ("""             
 		 update nnchannel set name = %s, lang = %s, sphere = %s
 		  where id = %s
      """, (cName, lang, sphere, cId)) 
   dbcontent.commit()
-  ### update category info
+  ### update category info                
   print "categoryName: " + categoryName
   contentCursor.execute("""
     select id 
@@ -77,14 +101,14 @@ for line in feed:
      """, (categoryName))
   categoryRow = contentCursor.fetchone()
   categoryId = categoryRow[0]
-  print "categoryId:" + str(categoryId)
+  print "categoryId:" + str(categoryId)        
   print "channelId:" + str(cId)
   contentCursor.execute("""
      select id
       from category_map
      where categoryId = %s and channelId = %s
      """, (categoryId, cId))
-  count = contentCursor.rowcount
+  count = contentCursor.rowcount          
   if count == 0:           
      contentCursor.execute("""
         insert into category_map (categoryId, channelId, updateDate)
@@ -107,7 +131,7 @@ for line in feed:
            insert into tag (name, updateDate)
                     values (%s, now()) 
            """, (t))                
-        contentCursor.execute("""
+        contentCursor.execute("""         
           select id 
             from tag
            where name = %s
@@ -116,7 +140,7 @@ for line in feed:
      tagId = tagRow[0]        
      contentCursor.execute("""
        select id
-         from tag_map
+         from tag_map                          
         where tagId = %s and channelId = %s
         """, (tagId, cId))
      count = contentCursor.rowcount
@@ -124,9 +148,9 @@ for line in feed:
         contentCursor.execute("""
            insert into tag_map (tagId, channelId, updateDate)
                         values (%s, %s, now()) 
-           """, (tagId, cId))
+           """, (tagId, cId))               
   dbcontent.commit()      
-  #if i > 2:
+  #if i > 2:                              
   #   break
 
 print "record done:" + str(i)
---
+++ installer/migration/trending.py
@@ -0,0 +1,45 @@
+#process billboard channels
+
+import urllib, urllib2
+import os
+from array import *
+import MySQLdb
+import time
+
+dbcontent = MySQLdb.connect (host = "localhost",
+                             user = "root",
+                             passwd = "",
+                             charset = "utf8",
+                             use_unicode = True,
+                             db = "nncloudtv_content")
+
+feed = open("trending_en.txt", "rU")
+################################
+i = 19
+for line in feed:
+  cId = line
+  contentCursor = dbcontent.cursor() 
+  contentCursor.execute("""
+      update tag_map
+         set channelId = %s
+       where id = %s
+      """, (cId, i))     
+  i = i+1
+dbcontent.commit()
+##################################
+feed = open("trending_zh.txt", "rU")
+i = 28
+for line in feed:
+  cId = line
+  contentCursor = dbcontent.cursor() 
+  contentCursor.execute("""
+      update tag_map
+         set channelId = %s
+       where id = %s
+      """, (cId, i))     
+  i = i+1
+dbcontent.commit()  
+contentCursor.close ()
+
+print "record done:" + str(i)
+feed.close()
