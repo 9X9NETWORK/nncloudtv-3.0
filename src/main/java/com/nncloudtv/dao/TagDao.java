1ecdcd0b1db9558f59e21537a9ea401cdf07121b
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2012-09-19 22:39:23 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/9x9dev/trunk/nncloudtv@3401 a0a5f996-2bb4-401c-8778-395c3c01700f

--- src/main/java/com/nncloudtv/dao/TagDao.java
+++ src/main/java/com/nncloudtv/dao/TagDao.java
@@ -30,6 +30,16 @@ public class TagDao extends GenericDao<Tag> {
         return tm;
     }
     
+    public void deleteTagMap(TagMap map) {
+        if (map == null) return;
+        PersistenceManager pm = PMF.get(map.getClass()).getPersistenceManager();
+        try {
+            pm.deletePersistent(map);
+        } finally {
+            pm.close();
+        }
+    }
+    
     public Tag findByName(String name) {
         PersistenceManager pm = PMF.getContent().getPersistenceManager();
         Tag detached = null;
@@ -47,6 +57,23 @@ public class TagDao extends GenericDao<Tag> {
         return detached;
     }
         
+    public List<TagMap> findMapByChannel(long chId) {
+        PersistenceManager pm = PMF.getContent().getPersistenceManager();
+        List<TagMap> detached = new ArrayList<TagMap>();
+        try {
+            Query query = pm.newQuery(TagMap.class);
+            query.setFilter("channelId == channelIdParam");
+            query.declareParameters("long channelIdParam");
+            @SuppressWarnings("unchecked")
+            List<TagMap> results = (List<TagMap>) query.execute(chId);
+            detached = (List<TagMap>)pm.detachCopyAll(results);            
+        } catch (JDOObjectNotFoundException e) {
+        } finally {
+            pm.close();
+        }
+        return detached;            	
+    }
+    
     public List<TagMap> findMapByTag(long tagId) {        
         PersistenceManager pm = PMF.getContent().getPersistenceManager();
         List<TagMap> detached = new ArrayList<TagMap>();
