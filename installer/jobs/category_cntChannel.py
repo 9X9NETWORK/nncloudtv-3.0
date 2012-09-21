6d9702d631074bc54a694639e5ae53cbad8f093a
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2012-09-21 00:12:49 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/9x9dev/trunk/nncloudtv@3438 a0a5f996-2bb4-401c-8778-395c3c01700f

---
+++ installer/jobs/category_cntChannel.py
@@ -0,0 +1,41 @@
+# count channel in the category
+
+import MySQLdb
+
+conn = MySQLdb.connect (host = "localhost",
+                        user = "root",
+                        passwd = "",
+                        charset = "utf8", 
+                        use_unicode = True,                                               
+                        db = "nncloudtv_content")
+
+try:
+  cursor = conn.cursor()
+  cursor.execute ("""  
+        select map.categoryId, count(*)
+         from category_map map, nnchannel c
+        where map.channelId = c.id
+          and c.isPublic=true
+          and c.status=0
+        group by map.categoryId  
+        order by map.categoryId                    
+     """)
+  data = cursor.fetchall ()
+  i = 1
+  for d in data:       
+    categoryId = d[0]    
+    cnt = d[1]
+    cursor.execute ("""            
+				 update category set channelCnt = %s
+				  where id = %s
+				 """, (cnt, categoryId))		          
+    i = i+ 1   
+  conn.commit()
+  cursor.close ()
+  conn.close ()
+
+  print "record done:" + str(i)
+
+except MySQLdb.Error, e:
+  print "Error %d: %s" % (e.args[0], e.args[1])
+  sys.exit (1)
