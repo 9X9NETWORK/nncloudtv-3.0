e98b0ccc6c22701997c05250f2c05571ffa1f453
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2012-09-22 05:02:02 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/9x9dev/trunk/nncloudtv@3463 a0a5f996-2bb4-401c-8778-395c3c01700f

---
+++ installer/jobs/channel_cntProgram.py
@@ -0,0 +1,36 @@
+import MySQLdb
+
+conn = MySQLdb.connect (host = "localhost",
+                        user = "root",
+                        passwd = "",
+                        charset = "utf8", 
+                        use_unicode = True,                                               
+                        db = "nncloudtv_content")
+
+
+cursor = conn.cursor()
+cursor.execute ("""
+   select c.id, count(*)
+     from nnchannel c, nnprogram p      
+    where (c.contentType = 6 || c.contentType = 8 || c.contentType = 9)
+      and c.id = p.channelId
+      and p.publishDate < now()
+      and p.isPublic=true
+    group by c.id
+   """)
+data = cursor.fetchall ()
+i = 1
+for d in data:       
+  cId = d[0]    
+  cnt = d[1]
+  cursor.execute ("""            
+			 update nnchannel set cntEpisode = %s
+			  where id = %s
+			 """, (cnt, cId))		          
+  i = i+ 1
+
+conn.commit()
+cursor.close()
+conn.close()
+print "record done:" + str(i)
+  
