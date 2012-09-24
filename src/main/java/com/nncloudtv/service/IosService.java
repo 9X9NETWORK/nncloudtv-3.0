3b02c1ab0687b9eb8c5da48122a02cce66723383
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2012-09-24 21:49:51 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/9x9dev/trunk/nncloudtv@3474 a0a5f996-2bb4-401c-8778-395c3c01700f

--- src/main/java/com/nncloudtv/service/IosService.java
+++ src/main/java/com/nncloudtv/service/IosService.java
@@ -10,6 +10,7 @@ import com.nncloudtv.lib.NnStringUtil;
 import com.nncloudtv.lib.YouTubeLib;
 import com.nncloudtv.model.LangTable;
 import com.nncloudtv.model.NnChannel;
+import com.nncloudtv.model.NnProgram;
 import com.nncloudtv.web.api.NnStatusCode;
 
 @Service
@@ -54,7 +55,8 @@ public class IosService {
     		String intro = c.getIntro();		
     		if (intro != null)
     			intro = intro.replaceAll("\n", " ").replaceAll("\t", " ");
-    		String imageUrl = c.getPlayerPrefImageUrl();	
+    		String imageUrl = c.getPlayerPrefImageUrl();
+    		imageUrl = imageUrl.indexOf("|") < 0 ? imageUrl : imageUrl.substring(0, imageUrl.indexOf("|"));     			    		
     		String youtubeId = "";
     		if (c.getSourceUrl() != null && c.getSourceUrl().contains("http://www.youtube.com"))
     			youtubeId = YouTubeLib.getYouTubeChannelName(c.getSourceUrl());
@@ -80,6 +82,104 @@ public class IosService {
     		result += NnStringUtil.getDelimitedStr(ori) + "\n";
 		}
 		return result;
+	}
+
+	private String composeLimitProgramInfoStr(String input, long sidx, long limit) {
+		if (sidx == 0 && limit == 0)
+			return input;
+		String[] lines = input.split("\n");
+		String result = "";
+		long start = sidx - 1;
+		long end = start + limit;
+		for (int i=0; i<lines.length; i++) {
+			if (i>=start && i<end) {
+				result += lines[i] + "\n";
+			}
+			if (i > end) {
+				return result;
+			}
+		}		
+		return result;
+	}
+
+    public String findPlayerProgramInfoByChannel(long channelId, long sidx, long limit) {
+        String result = this.findPlayerProgramInfoByChannel(channelId);
+        return this.composeLimitProgramInfoStr(result, sidx, limit);
+    }    
+	
+    public String findPlayerProgramInfoByChannel(long channelId) {
+    	log.info("request from != v32");
+		List<NnProgram> programs = new NnProgramManager().findPlayerProgramsByChannel(channelId);
+		log.info("channel id:" + channelId + "; program size:" + programs.size());
+		String str = this.composeProgramInfoStr(programs);
+		return str;
+    }    
+
+	public String composeProgramInfoStr(List<NnProgram> programs) {		
+		String output = "";		
+		String regexCache = "^(http|https)://(9x9cache.s3.amazonaws.com|s3.amazonaws.com/9x9cache)";
+		String regexPod = "^(http|https)://(9x9pod.s3.amazonaws.com|s3.amazonaws.com/9x9pod)";
+		String cache = "http://cache.9x9.tv";
+		String pod = "http://pod.9x9.tv";
+		for (NnProgram p : programs) {
+			//file urls
+			String url1 = p.getFileUrl();
+			String url2 = ""; //not used for now
+			String url3 = ""; //not used for now
+			String url4 = p.getAudioFileUrl();
+			String imageUrl = p.getImageUrl();
+			String imageLargeUrl = p.getImageUrl();
+			if (imageUrl == null) {imageUrl = "";}
+			if (imageLargeUrl == null) {imageLargeUrl = "";}
+		    if (url1 != null) {
+				url1 = url1.replaceFirst(regexCache, cache);
+				url1 = url1.replaceAll(regexPod, pod);
+		    }
+			url2 = url2.replaceFirst(regexCache, cache);
+			url2 = url2.replaceAll(regexPod, pod);
+			url3 = url3.replaceFirst(regexCache, cache);
+			url3 = url3.replaceAll(regexPod, pod);
+			if (url4 != null) {
+				url4 = url4.replaceFirst(regexCache, cache);
+				url4 = url4.replaceAll(regexPod, pod);
+			}
+			if (imageUrl != null) {
+				imageUrl = imageUrl.replaceFirst(regexCache, cache);
+				imageUrl = imageUrl.replaceAll(regexPod, pod);
+			}
+			if (imageLargeUrl != null) {
+				imageLargeUrl = imageLargeUrl.replaceFirst(regexCache, cache);
+				imageLargeUrl = imageLargeUrl.replaceAll(regexPod, pod);
+			}
+					
+			//intro
+			String intro = p.getIntro();			
+			if (intro != null) {
+				int introLenth = (intro.length() > 256 ? 256 : intro.length()); 
+				intro = intro.replaceAll("\\s", " ");				
+				intro = intro.substring(0, introLenth);
+			}
+			
+			//the rest
+			String[] ori = {String.valueOf(p.getChannelId()), 
+					        String.valueOf(p.getId()), 
+					        p.getName(), 
+					        intro,
+					        String.valueOf(p.getContentType()), 
+					        p.getDuration(),
+					        imageUrl,
+					        imageLargeUrl,
+					        url1, 
+					        url2, 
+					        url3, 
+					        url4,			
+					        String.valueOf(p.getUpdateDate().getTime()),
+					        p.getComment()};
+			output = output + NnStringUtil.getDelimitedStr(ori);
+			output = output.replaceAll("null", "");
+			output = output + "\n";
+		}
+		return output;		
 	}
 	
 }
