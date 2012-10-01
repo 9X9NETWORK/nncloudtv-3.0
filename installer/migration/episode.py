4673b9423d58a4906629c93140bdd1c6cca1ffc1
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2012-10-01 22:07:59 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/9x9dev/trunk/nncloudtv@3585 a0a5f996-2bb4-401c-8778-395c3c01700f

---
+++ installer/migration/episode.py
@@ -0,0 +1,39 @@
+# initialize tag information
+# 1) basic value for hot and thrending
+# 2) give channel tags
+
+import urllib, urllib2
+import os
+from array import *
+import MySQLdb
+import time
+import string
+import codecs
+
+dbcontent = MySQLdb.connect (host = "localhost",
+                             user = "root",
+                             passwd = "",
+                             charset = "utf8",
+                             use_unicode = True,
+                             db = "nncloudtv_content")
+cursor = dbcontent.cursor()
+cursor.execute("""truncate nnepisode""")
+cursor.execute("""
+  select p.name, p.imageUrl, p.intro, c.id, p.id
+    from nnprogram p, nnchannel c
+   where c.contentType = 6
+    and c.id = p.channelId
+   """)
+rows = cursor.fetchall()
+for r in rows:
+   cursor.execute("""
+      insert into nnepisode (name, imageUrl, intro, publishDate, updateDate, channelId)
+                     values (%s, %s, %s, now(), now(), %s) 
+       """, (r[0], r[1], r[2], r[3]))   
+   print str(cursor.lastrowid) + "; " + str(r[4])   
+   eid = cursor.lastrowid
+   cursor.execute("""update nnprogram set episodeId = %s
+                      where id = %s
+                   """, (eid, r[4]))
+dbcontent.commit()
+dbcontent.close()
