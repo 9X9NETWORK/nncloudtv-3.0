f93cf5fff3a0c2bffa7600ddb1a047fc42eeb445
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2012-10-11 21:06:05 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/9x9dev/trunk/nncloudtv@3708 a0a5f996-2bb4-401c-8778-395c3c01700f

--- installer/build.py
+++ installer/build.py
@@ -1,24 +1,20 @@
 import os, datetime, shutil
 
-#================================================================
-choice = raw_input('Environment (1. alpha 2. prod/stage) : ')
+#---- get environment ----  
+choice = raw_input('Environment (1. prod/stage) : ')
 server="prod"
-if choice == "1":
-  server="alpha"
-if choice == "2":                                          
-  server="prod"
 
-list=['datanucleus_analytics.properties', 'datanucleus_content.properties', 
-      'datanucleus_nnuser1.properties', 'datanucleus_nnuser2.properties', 'datanucleus_recommend.properties', 'aws.properties', 
+#---- copy property files ----
+list=['datanucleus_analytics.properties', 'datanucleus_content.properties',
+      'datanucleus_nnuser1.properties', 'datanucleus_nnuser2.properties', 'datanucleus_recommend.properties', 'aws.properties',
       'memcache.properties', 'queue.properties', 'sns.properties', 'piwik.properties']
 
-for l in list:                                                                         
-   src = server + "/" + l                                                
+for l in list:
+   src = server + "/" + l
    dst = "../src/main/resources/" + l
    shutil.copyfile(src, dst)
 
-
-#================================================================                         
+#---- write version info ----
 version = raw_input('Enter version number : ')
 source = open(".svn//entries", "rU")
 cnt = 0
@@ -34,30 +30,29 @@ print now
 old_file = open("..//src//main//java//com//nncloudtv//web//VersionController.java", "rU")
 new_file = open("..//src//main//java//com//nncloudtv//web//VersionController.java.tmp",'w')
 for line in old_file:
-  if (line.find("String appVersion") > 0):   
+  if (line.find("String appVersion") > 0):
      line = "        String appVersion = \"" + version + "\";\n"
-  if (line.find("String svn") > 0):   
-     line = "        String svn = \"" + rev + "\";\n"	
-  if (line.find("String packagedTime") > 0):   
-     line = "        String packagedTime = \"" + str(now) + "\";\n"
+  if (line.find("String svn") > 0):
+     line = "        String svn = \"" + rev + "\";\n"
+  if (line.find("String packagedTime") > 0):
   new_file.write(line)
 old_file.close()
 new_file.close()
-                                                            
+
 os.remove("..//src//main//java//com//nncloudtv//web//VersionController.java")
 os.rename("..//src//main//java//com//nncloudtv//web//VersionController.java.tmp", "..//src//main//java//com//nncloudtv//web//VersionController.java")
 
-#================================================================
-os.chdir("..//")                                                           
-os.system("mvn clean compile")                                                 
+#---- build ----
+os.chdir("..//")
+os.system("mvn clean compile")
 os.system("mvn datanucleus:enhance")
 os.system("mvn war:war")
 
-os.chdir("..//nncms")                                                           
-os.system("mvn clean compile")                                                 
+os.chdir("..//nncms")
+os.system("mvn clean compile")
 os.system("mvn war:war")
 
-
+#---- summary ----
 print "\n--- summary ---\n"
 print "Package environment:" + server
 print "Version number:" + version
