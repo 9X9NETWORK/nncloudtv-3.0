324d0b90df00c6dc67417a722835996bc0e61c89
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2012-10-11 20:45:46 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/9x9dev/trunk/nncloudtv@3705 a0a5f996-2bb4-401c-8778-395c3c01700f

--- installer/upload.py
+++ installer/upload.py
@@ -1,16 +1,28 @@
 import shutil, os, re, sys, smtplib, commands
+import paramiko
 from email.MIMEText import MIMEText
 from datetime import datetime
-
+                                               
 # it does
-#   1.copies over root.war
+#   1.copies over root.war             
 #   2.write md5 to root.md5 file
 #   3.write version file
 #   4.upload files to server:
-#     prerequisites: your own key files, pscp(change to your own scp programs)
+#     prerequisites: your own key files
 
-myos = raw_input('OS (1.Ubuntu 2.Windows) : ')
+def getSSH(server):
+   host = "stage.9x9.tv"
+   if server == "prod":    
+     host = "moveout-log.9x9.tv"     
+   if server == "stage":
+     privatekeyfile = os.path.expanduser('~/keys/prod-west2.pem')
+     mykey = paramiko.RSAKey.from_private_key_file(privatekeyfile)
+     ssh = paramiko.SSHClient()
+     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+     ssh.connect(host, username = 'ubuntu', pkey = mykey)
+     return ssh
 
+#--------------- write version file
 myfile = open("..//src//main//java//com//nncloudtv//web//VersionController.java", "rU")
 version = ""
 for line in myfile:
@@ -21,11 +33,10 @@ for line in myfile:
 
 if version == "":
   version = raw_input('Enter version number : ') 
-
 print "version number:" + version
 
-# copy over root.war
-src = "../target/root.war"
+#--------------- copy war files       
+src = "../target/root.war"         
 dst = "root.war"
 shutil.copyfile(src, dst)                                                                                       
 
@@ -33,18 +44,14 @@ src = "../../nncms/target/cms.war"
 dst = "cms.war"
 shutil.copyfile(src, dst)                                                                                       
 
-print "--- generate root.war ---"
-
-# generate md5
-if myos == "1":
-  md5 = os.popen("md5sum root.war").read()
-  cmsMd5 = os.popen("md5sum cms.war").read()
-if myos == "2":
-  md5 = os.popen("tools\md5sums -u root.war").read()
-match = re.match("(.*)( .*)", md5)
-cmsmatch = re.match("(.*)( .*)", md5)
+#--------------- generate md5            
+md5 = os.popen("md5sum root.war").read()
+match = re.match("(.*)( .*)", md5)       
 if match:
   md5 = match.group(1)
+
+cmsMd5 = os.popen("md5sum cms.war").read()
+cmsmatch = re.match("(.*)( .*)", md5)
 if cmsmatch:
   cmsMd5 = cmsmatch.group(1)
 
@@ -59,64 +66,61 @@ dest.close()
 dest = open("cms.md5", "w")
 line = md5 + " " + "cms.war\x00\x0a"
 dest.write(line)
-dest.close()
+dest.close()                           
 
-# write version file
+#--------------- generate  version file              
+print "--- generate version file ---"
 dest = open("version", "w")
 line = version
 dest.write(line)
 dest.close()
-print "--- generate version file ---"
 
-# upload to server
+#--------------- upload to server
 print "\n"                                             
-server = raw_input('Server (1.devel1 2.alpha 3.stage 4.deploy 5.exit) : ')
-if server != "5":
-  print "--- " + datetime.now().strftime("%d/%m/%y %H:%M:%S %Z") + "---"
-if server == "1":
-  print "--- uploading to devel1 server ---"
-  if myos == "1":
-    os.system("scp -i ~/keys/dev-west2.pem root.war ubuntu@ec2-50-112-54-59.us-west-2.compute.amazonaws.com:/home/ubuntu/files/root.war")
-  if myos == "2":
-    os.system("pscp -i dev-west2.ppk root.war ubuntu@ec2-50-112-54-59.us-west-2.compute.amazonaws.com:/home/ubuntu/files")
-if server == "2":
-  print "--- uploading to alpha server ---"
-  folder = raw_input('Folder created? : ')  # to be hooked up with ssh command, used as reminder for now
-  if myos == "1":            
-    os.system("scp -i ~/keys/dev-west2.pem root.war ubuntu@alpha_log.9x9.tv:/var/www/updates/" + version + "/root.war")
-    os.system("scp -i ~/keys/dev-west2.pem root.md5 ubuntu@alpha_log.9x9.tv:/var/www/updates/" + version + "/root.md5")
-    os.system("scp -i ~/keys/dev-west2.pem version ubuntu@alpha_log.9x9.tv:/var/www/updates/version")  
-    os.system("scp -i ~/keys/dev-west2.pem version ubuntu@alpha_log.9x9.tv:/var/www/updates/" + version + "/version")
-  if myos == "2":           
-     os.system("pscp -i dev-west2.ppk root.war ubuntu@alpha_log.9x9.tv:/var/www/updates/" + version)
-     os.system("pscp -i dev-west2.ppk root.md5 ubuntu@alpha_log.9x9.tv:/var/www/updates/" + version)
-     os.system("pscp -i dev-west2.ppk version ubuntu@alpha_log.9x9.tv:/var/www/updates")  
-     os.system("pscp -i dev-west2.ppk version ubuntu@alpha_log.9x9.tv:/var/www/updates/" + version)
-if server == "3":
-  print "--- uploading to stage server ---"
-  if myos == "1":             
-    os.system("scp -i ~/keys/prod-west2.pem root.war ubuntu@ec2-50-112-111-245.us-west-2.compute.amazonaws.com:/home/ubuntu/files/root.war")
-  if myos == "2": 
-    os.system("pscp -i prod-west2.ppk root.war ubuntu@ec2-50-112-111-245.us-west-2.compute.amazonaws.com:/home/ubuntu/files")
-if server == "4":
-  print "--- uploading to deploy server ---"
-  folder = raw_input('Folder created? : ')  # to be hooked up with ssh command, used as reminder for now
-  if myos == "1":                  
-     #os.system("scp -i ~/keys/prod-west2.pem root.war ubuntu@ec2-50-112-96-199.us-west-2.compute.amazonaws.com:/var/www/updates/" + version + "/root.war")
-     #os.system("scp -i ~/keys/prod-west2.pem root.md5 ubuntu@ec2-50-112-96-199.us-west-2.compute.amazonaws.com:/var/www/updates/" + version + "/root.md5")
-     #os.system("scp -i ~/keys/prod-west2.pem version ubuntu@ec2-50-112-96-199.us-west-2.compute.amazonaws.com:/var/www/updates/version")  
-     #os.system("scp -i ~/keys/prod-west2.pem version ubuntu@ec2-50-112-96-199.us-west-2.compute.amazonaws.com:/var/www/updates/" + version + "/version")
-     
-     #os.system("scp -i ~/keys/prod-west2.pem cms.war ubuntu@ec2-50-112-96-199.us-west-2.compute.amazonaws.com:/var/www/updates/cms/cms.war")
-     #os.system("scp -i ~/keys/prod-west2.pem cms.md5 ubuntu@ec2-50-112-96-199.us-west-2.compute.amazonaws.com:/var/www/updates/cms/cms.md5")
-     
-     os.system("scp -i ~/keys/prod-west2.pem cms.war ubuntu@v32a.9x9.tv:/usr/share/jetty/webapps/cms.war")
-     os.system("scp -i ~/keys/prod-west2.pem root.war ubuntu@v32a.9x9.tv:/usr/share/jetty/webapps/root.war")
+server = raw_input('Server (1.stage 2.prod 3.exit) : ')
+if server == "1":      
+  print "--- uploading to stage server ---"             
+  os.system("scp -i ~/keys/prod-west2.pem root.war ubuntu@stage.9x9.tv:/home/ubuntu/files/root.war")
+  os.system("scp -i ~/keys/prod-west2.pem root.war ubuntu@stage.9x9.tv:/home/ubuntu/files/cms.war")
+if server == "2":        
+  print "--- uploading to deploy server ---"                             
+  ssh = getSSH("prod")
+  
+  stdin, stdout, stderr = ssh.exec_command('mkdir /var/www/updates/root/' + version)
+  stdin, stdout, stderr = ssh.exec_command('mkdir /var/www/updates/cms/' + version)  
+  print "[moveout-log] ls /var/www/updates/root"
+  stdin, stdout, stderr = ssh.exec_command('ls /var/www/updates/root/')
+  print stdout.readlines()
+  print "[moveout-log] ls /var/www/updates/cms"
+  stdin, stdout, stderr = ssh.exec_command('ls /var/www/updates/cms/')
+  print stdout.readlines()
+  ssh.close()                                  
+                                  
+  os.system("scp -i ~/keys/prod-west2.pem root.war ubuntu@moveout-log.9x9.tv:/var/www/updates/root/" + version + "/root.war")
+  os.system("scp -i ~/keys/prod-west2.pem root.md5 ubuntu@moveout-log.9x9.tv:/var/www/updates/root/" + version + "/root.md5")
+  os.system("scp -i ~/keys/prod-west2.pem version ubuntu@moveout-log.9x9.tv:/var/www/updates/root/version")
+  os.system("scp -i ~/keys/prod-west2.pem version ubuntu@moveout-log.9x9.tv:/var/www/updates/root/" + version + "/version")
+                                  
+  os.system("scp -i ~/keys/prod-west2.pem cms.war ubuntu@moveout-log.9x9.tv:/var/www/updates/cms/" + version + "/cms.war")
+  os.system("scp -i ~/keys/prod-west2.pem cms.md5 ubuntu@moveout-log.9x9.tv:/var/www/updates/cms/" + version + "/cms.md5")
+  os.system("scp -i ~/keys/prod-west2.pem version ubuntu@moveout-log.9x9.tv:/var/www/updates/cms/version")
+  os.system("scp -i ~/keys/prod-west2.pem version ubuntu@moveout-log.9x9.tv:/var/www/updates/cms/" + version + "/version")
 
-if myos == "2": 
-     os.system("pscp -i prod-west2.ppk root.war ubuntu@ec2-50-112-96-199.us-west-2.compute.amazonaws.com:/var/www/updates/" + version)
-     os.system("pscp -i prod-west2.ppk root.md5 ubuntu@ec2-50-112-96-199.us-west-2.compute.amazonaws.com:/var/www/updates/" + version)
-     os.system("pscp -i prod-west2.ppk version ubuntu@ec2-50-112-96-199.us-west-2.compute.amazonaws.com:/var/www/updates")  
-     os.system("pscp -i prod-west2.ppk version ubuntu@ec2-50-112-96-199.us-west-2.compute.amazonaws.com:/var/www/updates/" + version)
-
-print "--- " + datetime.now().strftime("%d/%m/%y %H:%M:%S %Z") + "---"  
+#--------------- deploy
+print "\n"                                                       
+server = raw_input('Deploy? (1.stage 2.prod 3.exit) : ')
+if server == "1":      
+  print "--- deploying on stage server ---"
+  ssh = getSSH("stage")
+  stdin, stdout, stderr = ssh.exec_command('cd files')
+  stdin, stdout, stderr = ssh.exec_command('sh installer.sh')
+  print stdout.readlines()                                                   
+  ssh.close()
+if server == "2":  
+  print "--- deploying to prod server ---"
+  ssh = getSSH("prod")
+  stdin, stdout, stderr = ssh.exec_command('cd bin/v32')
+  stdin, stdout, stderr = ssh.exec_command('./deploy_all_wars.sh')
+  print stdout.readlines()
+  ssh.close()                                  
+  
