6a4bc4f3bd9f068d9a05c87705f774f3f41867c2
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2012-10-12 19:38:18 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/9x9dev/trunk/nncloudtv@3736 a0a5f996-2bb4-401c-8778-395c3c01700f

---
+++ installer/migration/episode_duration.py
@@ -0,0 +1,38 @@
+# initialize tag information
+# 1) basic value for hot and thrending
+# 2) give channel tags
+
+import urllib, urllib2
+import os
+from array import *
+import MySQLdb
+import time
+import string
+import codecs
+
+dbcontent = MySQLdb.connect (host = "localhost",
+                             user = "root",
+                             passwd = "",
+                             charset = "utf8",
+                             use_unicode = True,
+                             db = "nncloudtv_content")
+cursor = dbcontent.cursor()
+cursor.execute("""
+  select id
+    from nnepisode
+   where duration = 0
+   """)
+rows = cursor.fetchall()
+i=0
+for r in rows:
+   i = i + 1
+   eid = r[0]
+   url = "http://localhost:8080/playerAPI/episodeUpdate?epId=" + str(eid)
+   print url
+   urllib2.urlopen(url).read()
+
+   print "eid:" + str(eid) 
+   if i > 5:
+      break
+
+dbcontent.close()
