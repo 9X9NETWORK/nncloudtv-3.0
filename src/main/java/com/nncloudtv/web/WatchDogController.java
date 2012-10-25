5416683b1f47f97c210b59331e6420e7d74754e5
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2012-10-25 22:53:26 +0000

    add programInfo debugger
    git-svn-id: https://everest.teltel.com:8443/repos/misc/9x9dev/trunk/nncloudtv@3851 a0a5f996-2bb4-401c-8778-395c3c01700f

--- src/main/java/com/nncloudtv/web/WatchDogController.java
+++ src/main/java/com/nncloudtv/web/WatchDogController.java
@@ -7,10 +7,13 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.nncloudtv.lib.NnNetUtil;
 import com.nncloudtv.model.Mso;
 import com.nncloudtv.service.MsoManager;
+import com.nncloudtv.service.NnProgramManager;
 import com.nncloudtv.service.PlayerApiService;
 import com.nncloudtv.web.api.NnStatusCode;
 
@@ -38,4 +41,42 @@ public class WatchDogController {
         return NnNetUtil.textReturn(output);
     }    
 
+    @RequestMapping(value="programInfo", produces = "text/plain; charset=utf-8")
+    public @ResponseBody String programInfo(
+            @RequestParam(value="channel", required=false) String channel) {
+        NnProgramManager mngr = new NnProgramManager();
+        String result = mngr.findPlayerProgramInfoByChannel(Long.parseLong(channel));        
+        if (result == null)
+            return "null, error";
+        String output = "";
+        if (result != null) {
+            String[] lines = result.split("\n");
+            output += lines.length + " program records. \n--\n";
+            for (String l : lines) {
+                String[] data = l.split("\t");
+                for (int i=0; i < data.length; i++) {
+                    if (i == 0)  output += "channel id:";
+                    if (i == 1)  output += "program id:"; 
+                    if (i == 2)  output += "name:";
+                    if (i == 3)  output += "description:";
+                    if (i == 4)  output += "content type:";
+                    if (i == 5)  output += "duration:";
+                    if (i == 6)  output += "image url:";
+                    if (i == 7)  output += "image large url:";
+                    if (i == 8)  output += "video url:";
+                    if (i == 9)  output += "url2:";
+                    if (i == 10) output += "url3:";
+                    if (i == 11) output += "audior url:";
+                    if (i == 12) output += "last update time:";
+                    if (i == 13) output += "comment:";
+                    if (i == 14) output += "title card:";
+                    output += data[i] + "\n";
+                }
+                output += "--\n";
+            }
+        }
+                    
+        return output;        
+    }
+    
 }
