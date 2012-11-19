faf77d410282b87b8834d58e9ad7f652ec1b3a48
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2012-11-19 21:04:54 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/9x9dev/trunk/nncloudtv@4035 a0a5f996-2bb4-401c-8778-395c3c01700f

---
+++ installer/jobs/channel_subscriber.py
@@ -0,0 +1,44 @@
+# update channel subscriber count
+import MySQLdb
+import sqlite3
+
+conn = MySQLdb.connect (host = "localhost",
+                        user = "root",
+                        passwd = "",
+                        charset = "utf8",
+                        use_unicode = True,
+                        db = "nncloudtv_content")
+
+try:
+  cursor = conn.cursor()
+  #TODO nnuser2 table as well
+  cursor.execute("""
+    select id from nnchannel
+      """)
+
+  rows = cursor.fetchall()
+  for row in rows:
+    cid = row[0]
+    cursor.execute("""
+      select distinct userId from nncloudtv_nnuser1.nnuser_subscribe where channelId = 2 order by rand() limit 3;
+        """)
+    user_rows = cursor.fetchall()
+    uidStr = ""
+    for ur in user_rows:
+       uid = ur[0]
+       uidStr = uidStr + str(uid) + ";"
+    if len(uidStr) > 0:
+       print "update ch " + str(cid) + " with uids: " + uidStr
+       cursor.execute("""
+         update nnchannel set subscribersIdStr = %s where id = %s
+           """, (uidStr, cid))
+       
+  conn.commit()
+  cursor.close ()
+  conn.close()
+
+except MySQLdb.Error, e:
+  print "Error %d: %s" % (e.args[0], e.args[1])
+  sys.exit (1)
+
+
