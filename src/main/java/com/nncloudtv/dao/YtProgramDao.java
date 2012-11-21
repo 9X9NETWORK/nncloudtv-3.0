6091c5344abbb7ddb8834d11f5575fbcdd873ae7
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2012-11-21 22:02:01 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/9x9dev/trunk/nncloudtv@4052 a0a5f996-2bb4-401c-8778-395c3c01700f

---
+++ src/main/java/com/nncloudtv/dao/YtProgramDao.java
@@ -0,0 +1,49 @@
+package com.nncloudtv.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+
+import com.nncloudtv.lib.PMF;
+import com.nncloudtv.model.NnChannel;
+import com.nncloudtv.model.YtProgram;
+
+public class YtProgramDao extends GenericDao<YtProgram> {
+    
+    protected static final Logger log = Logger.getLogger(YtProgramDao.class.getName());    
+        
+    public YtProgramDao() {
+        super(YtProgram.class);
+    }
+    
+    public List<YtProgram> findByChannels(List<NnChannel> channels) {
+        List<YtProgram> detached = new ArrayList<YtProgram>();
+        String ids = "";
+        for (NnChannel c : channels) {
+            ids += "," + String.valueOf(c.getId());
+        }
+        if (ids.length() == 0) return detached;
+        if (ids.length() > 0) ids = ids.replaceFirst(",", "");
+        log.info("find in these channels:" + ids);
+        PersistenceManager pm = PMF.getContent().getPersistenceManager();
+        try {
+            String sql = "select * " +
+                           "from ytprogram " +
+                         " where channelId in (" + ids + ") " + 
+                         " order by updateDate desc limit 50";
+            log.info("sql:" + sql);
+            Query query = pm.newQuery("javax.jdo.query.SQL", sql);
+            query.setClass(YtProgram.class);
+            @SuppressWarnings("unchecked")
+            List<YtProgram> results = (List<YtProgram>) query.execute();
+            detached = (List<YtProgram>)pm.detachCopyAll(results);
+        } finally {
+            pm.close();
+        } 
+        return detached;        
+    }
+    
+}
