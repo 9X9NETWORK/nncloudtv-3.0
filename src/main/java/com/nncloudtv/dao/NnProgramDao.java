a863ac81841423d8bdbdb172604ff8f170a3607d
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2012-12-05 20:11:17 +0000

    player program picked only status!=1
    git-svn-id: https://everest.teltel.com:8443/repos/misc/9x9dev/trunk/nncloudtv@4095 a0a5f996-2bb4-401c-8778-395c3c01700f

--- src/main/java/com/nncloudtv/dao/NnProgramDao.java
+++ src/main/java/com/nncloudtv/dao/NnProgramDao.java
@@ -252,7 +252,7 @@ public class NnProgramDao extends GenericDao<NnProgram> {
                            "from nnprogram " +
                          " where episodeId in " +
                           "(select id from nnepisode where channelId = " + channelId + " and isPublic=true order by seq) " +
-                          " and status = 0 " + 
+                          " and status != " + NnProgram.STATUS_ERROR + " " + 
                           "order by seq, subSeq"; //TODO, order by seq is wrong
             log.info("sql:" + sql);
             Query query = pm.newQuery("javax.jdo.query.SQL", sql);
