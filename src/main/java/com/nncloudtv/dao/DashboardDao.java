ac708a87634016e93017b1237ca6b6d99b23e0a3
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2012-12-14 07:13:12 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/9x9dev/trunk/nncloudtv@4132 a0a5f996-2bb4-401c-8778-395c3c01700f

---
+++ src/main/java/com/nncloudtv/dao/DashboardDao.java
@@ -0,0 +1,54 @@
+package com.nncloudtv.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+
+import com.nncloudtv.lib.PMF;
+import com.nncloudtv.model.Dashboard;
+
+public class DashboardDao  extends GenericDao<Dashboard> {
+    
+    public DashboardDao() {
+        super(Dashboard.class);
+    }
+
+    public List<Dashboard> findFrontpage(short baseTime) {
+        PersistenceManager pm = PMF.getContent().getPersistenceManager();
+        List<Dashboard> detached = new ArrayList<Dashboard>();
+        try {
+            String sql = "select * from dashboard " +
+            		     " where ((timeStart != 0 or timeEnd != 0) and timeEnd > " + baseTime + " and timeStart <= " + baseTime + ")" +
+            		     " or (timeStart = 0 and timeEnd = 0) order by seq";
+            log.info("Sql=" + sql);
+            Query q= pm.newQuery("javax.jdo.query.SQL", sql);
+            q.setClass(Dashboard.class);
+            @SuppressWarnings("unchecked")
+            List<Dashboard> results = (List<Dashboard>) q.execute();
+            detached = (List<Dashboard>)pm.detachCopyAll(results);
+        } finally {
+            pm.close();
+        }
+        return detached;        
+    } 
+       
+    /*
+    public List<Dashboard> findFrontpage(short baseTime) {
+        PersistenceManager pm = PMF.getContent().getPersistenceManager();
+        List<Dashboard> detached = new ArrayList<Dashboard>();
+        try {
+            Query q = pm.newQuery(Dashboard.class);
+            @SuppressWarnings("unchecked")
+            List<Dashboard> items = (List<Dashboard>) q.execute();
+            detached = (List<Dashboard>)pm.detachCopyAll(items);
+        } finally {
+            pm.close();
+        }
+        return detached;        
+    } 
+    */
+    
+    
+}
