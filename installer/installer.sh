7f14b4c0c4df4603167d1cb3cf9d6a647c579cc6
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2013-03-02 02:16:14 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/9x9dev/trunk/nncloudtv@4333 a0a5f996-2bb4-401c-8778-395c3c01700f

--- installer/installer.sh
+++ installer/installer.sh
@@ -1,13 +1,15 @@
-#ec2 devel deployment script, not intended for production
-clear
-echo "install"
-md5sum root.war
-service jetty stop
-rm /usr/share/jetty/webapps/root.war
-cp root.war /usr/share/jetty/webapps/root.war
-service jetty start
-sleep 3
-wget http://localhost:8080/version/current
-sleep 1
-cat current
-rm current
+o "install"
+sudo service jetty stop
+sudo service memcached stop
+cd nncloudtv/
+mvn clean compile
+mvn datanucleus:enhance
+mvn war:war
+cd ../
+cd nncms/
+mvn clean compile war:war
+cd ../
+sudo cp nncloudtv/target/root.war /usr/share/jetty/webapps/root.war
+sudo cp nncms/target/cms.war /usr/share/jetty/webapps/cms.war
+sudo service jetty start
+sudo service memcached start
