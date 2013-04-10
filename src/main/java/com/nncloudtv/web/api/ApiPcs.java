3f8e1c70cc2da7f1f4118f4b313aa0ecb8057e49
barton <barton@a0a5f996-2bb4-401c-8778-395c3c01700f>
2013-04-10 09:59:52 +0000

    PCS controller created
    git-svn-id: https://everest.teltel.com:8443/repos/misc/9x9dev/trunk/nncloudtv@4471 a0a5f996-2bb4-401c-8778-395c3c01700f

---
+++ src/main/java/com/nncloudtv/web/api/ApiPcs.java
@@ -0,0 +1,414 @@
+package com.nncloudtv.web.api;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.logging.Logger;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.nncloudtv.model.Mso;
+import com.nncloudtv.model.NnChannel;
+import com.nncloudtv.service.MsoManager;
+import com.nncloudtv.service.NnChannelManager;
+
+@Controller
+@RequestMapping("api")
+public class ApiPcs extends ApiGeneric {
+    
+    protected static Logger log = Logger.getLogger(ApiPcs.class.getName());
+    
+    private MsoManager msoMngr;
+    private NnChannelManager channelMngr;
+    
+    @Autowired
+    public ApiPcs(MsoManager msoMngr, NnChannelManager channelMngr) {
+        this.msoMngr = msoMngr;
+        this.channelMngr = channelMngr;
+    }
+    
+    @RequestMapping(value = "msos/{msoId}/sets", method = RequestMethod.GET)
+    public @ResponseBody
+    List<Map<String, Object>> msoSets(HttpServletRequest req,
+            HttpServletResponse resp, @PathVariable("msoId") String msoIdStr) {
+        
+        Long msoId = null;
+        try {
+            msoId = Long.valueOf(msoIdStr);
+        } catch (NumberFormatException e) {
+        }
+        if (msoId == null) {
+            notFound(resp, INVALID_PATH_PARAMETER);
+            return null;
+        }
+        
+        Mso mso = msoMngr.findById(msoId);
+        if (mso == null) {
+            notFound(resp, "Mso Not Found");
+            return null;
+        }
+        
+        List<Map<String, Object>> results = new ArrayList<Map<String, Object>>();
+        Map<String, Object> result = new TreeMap<String, Object>();
+        
+        results.add(result);
+        
+        return results;
+    }
+    
+    @RequestMapping(value = "msos/{msoId}/sets", method = RequestMethod.POST)
+    public @ResponseBody
+    Map<String, Object> msoSetCreate(HttpServletRequest req,
+            HttpServletResponse resp, @PathVariable("msoId") String msoIdStr) {
+        
+        Long msoId = null;
+        try {
+            msoId = Long.valueOf(msoIdStr);
+        } catch (NumberFormatException e) {
+        }
+        if (msoId == null) {
+            notFound(resp, INVALID_PATH_PARAMETER);
+            return null;
+        }
+        
+        Mso mso = msoMngr.findById(msoId);
+        if (mso == null) {
+            notFound(resp, "Mso Not Found");
+            return null;
+        }
+        
+        Map<String, Object> result = new TreeMap<String, Object>();
+        
+        return result;
+    }
+    
+    @RequestMapping(value = "sets/{setId}", method = RequestMethod.GET)
+    public @ResponseBody
+    Map<String, Object> set(HttpServletRequest req,
+            HttpServletResponse resp, @PathVariable("setId") String setIdStr) {
+        
+        Long setId = null;
+        try {
+            setId = Long.valueOf(setIdStr);
+        } catch (NumberFormatException e) {
+        }
+        if (setId == null) {
+            notFound(resp, INVALID_PATH_PARAMETER);
+            return null;
+        }
+        
+        Map<String, Object> result = new TreeMap<String, Object>();
+        
+        return result;
+    }
+    
+    @RequestMapping(value = "sets/{setId}", method = RequestMethod.PUT)
+    public @ResponseBody
+    Map<String, Object> setUpdate(HttpServletRequest req,
+            HttpServletResponse resp, @PathVariable("setId") String setIdStr) {
+        
+        Long setId = null;
+        try {
+            setId = Long.valueOf(setIdStr);
+        } catch (NumberFormatException e) {
+        }
+        if (setId == null) {
+            notFound(resp, INVALID_PATH_PARAMETER);
+            return null;
+        }
+        
+        Map<String, Object> result = new TreeMap<String, Object>();
+        
+        return result;
+    }
+    
+    @RequestMapping(value = "sets/{setId}", method = RequestMethod.DELETE)
+    public @ResponseBody
+    String setDelete(HttpServletRequest req,
+            HttpServletResponse resp, @PathVariable("setId") String setIdStr) {
+        
+        Long setId = null;
+        try {
+            setId = Long.valueOf(setIdStr);
+        } catch (NumberFormatException e) {
+        }
+        if (setId == null) {
+            notFound(resp, INVALID_PATH_PARAMETER);
+            return null;
+        }
+        
+        okResponse(resp);
+        return null;
+    }
+    
+    @RequestMapping(value = "sets/{setId}/channels", method = RequestMethod.GET)
+    public @ResponseBody
+    List<NnChannel> setChannels(HttpServletRequest req,
+            HttpServletResponse resp, @PathVariable("setId") String setIdStr) {
+        
+        Long setId = null;
+        try {
+            setId = Long.valueOf(setIdStr);
+        } catch (NumberFormatException e) {
+        }
+        if (setId == null) {
+            notFound(resp, INVALID_PATH_PARAMETER);
+            return null;
+        }
+        
+        List<NnChannel> results = new ArrayList<NnChannel>();
+        
+        return results;
+    }
+    
+    @RequestMapping(value = "sets/{setId}/channels", method = RequestMethod.POST)
+    public @ResponseBody
+    String setChannelAdd(HttpServletRequest req,
+            HttpServletResponse resp, @PathVariable("setId") String setIdStr) {
+        
+        Long setId = null;
+        try {
+            setId = Long.valueOf(setIdStr);
+        } catch (NumberFormatException e) {
+        }
+        if (setId == null) {
+            notFound(resp, INVALID_PATH_PARAMETER);
+            return null;
+        }
+        
+        Long channelId = null;
+        String channelIdStr = req.getParameter("channelId");
+        if (channelIdStr != null) {
+            try {
+                channelId = Long.valueOf(channelIdStr);
+            } catch (NumberFormatException e) {
+            }
+            if (channelId == null) {
+                badRequest(resp, INVALID_PARAMETER);
+                return null;
+            }
+        } else {
+            badRequest(resp, MISSING_PARAMETER);
+            return null;
+        }
+        
+        NnChannel channel = null;
+        channel = channelMngr.findById(channelId);
+        if (channel == null) {
+            badRequest(resp, "Channel Not Found");
+            return null;
+        }
+        
+        okResponse(resp);
+        return null;
+    }
+    
+    @RequestMapping(value = "sets/{setId}/channels", method = RequestMethod.DELETE)
+    public @ResponseBody
+    String setChannelRemove(HttpServletRequest req,
+            HttpServletResponse resp, @PathVariable("setId") String setIdStr) {
+        
+        Long setId = null;
+        try {
+            setId = Long.valueOf(setIdStr);
+        } catch (NumberFormatException e) {
+        }
+        if (setId == null) {
+            notFound(resp, INVALID_PATH_PARAMETER);
+            return null;
+        }
+        
+        Long channelId = null;
+        String channelIdStr = req.getParameter("channelId");
+        if (channelIdStr != null) {
+            try {
+                channelId = Long.valueOf(channelIdStr);
+            } catch (NumberFormatException e) {
+            }
+            if (channelId == null) {
+                badRequest(resp, INVALID_PARAMETER);
+                return null;
+            }
+        } else {
+            badRequest(resp, MISSING_PARAMETER);
+            return null;
+        }
+        
+        NnChannel channel = null;
+        channel = channelMngr.findById(channelId);
+        if (channel == null) {
+            badRequest(resp, "Channel Not Found");
+            return null;
+        }
+        
+        okResponse(resp);
+        return null;
+    }
+    
+    @RequestMapping(value = "msos/{msoId}/store", method = RequestMethod.GET)
+    public @ResponseBody
+    List<NnChannel> storeChannels(HttpServletRequest req,
+            HttpServletResponse resp, @PathVariable("msoId") String msoIdStr) {
+        
+        Long msoId = null;
+        try {
+            msoId = Long.valueOf(msoIdStr);
+        } catch (NumberFormatException e) {
+        }
+        if (msoId == null) {
+            notFound(resp, INVALID_PATH_PARAMETER);
+            return null;
+        }
+        
+        Mso mso = msoMngr.findById(msoId);
+        if (mso == null) {
+            notFound(resp, "Mso Not Found");
+            return null;
+        }
+        
+        // channels
+        String channelIdsStr = req.getParameter("channels");
+        
+        // paging
+        long page = 0, rows = 0;
+        try {
+            String pageStr = req.getParameter("page");
+            String rowsStr = req.getParameter("rows");
+            if (pageStr != null && rowsStr != null) {
+                page = Long.valueOf(pageStr);
+                rows = Long.valueOf(rowsStr);
+            }
+        } catch (NumberFormatException e) {
+        }
+        
+        List<NnChannel> results = new ArrayList<NnChannel>();
+        if (channelIdsStr != null) { // find by filter
+            String[] channelIdStrList = channelIdsStr.split(",");
+            List<Long> channelIdList = new ArrayList<Long>();
+            Long channelId = null;
+            for (String channelIdStr : channelIdStrList) {
+                
+                channelId = null;
+                try {
+                    channelId = Long.valueOf(channelIdStr);
+                } catch(Exception e) {
+                }
+                if (channelId != null) {
+                    channelIdList.add(channelId);
+                }
+            }
+            // use channelIdList to do something
+            
+        } else if ((page > 0) && (rows > 0)) { // find by paging
+            
+        } else { // find all
+            
+        }
+        
+        return results;
+    }
+    
+    @RequestMapping(value = "msos/{msoId}/store", method = RequestMethod.POST)
+    public @ResponseBody
+    String storeChannelAdd(HttpServletRequest req,
+            HttpServletResponse resp, @PathVariable("msoId") String msoIdStr) {
+        
+        Long msoId = null;
+        try {
+            msoId = Long.valueOf(msoIdStr);
+        } catch (NumberFormatException e) {
+        }
+        if (msoId == null) {
+            notFound(resp, INVALID_PATH_PARAMETER);
+            return null;
+        }
+        
+        Mso mso = msoMngr.findById(msoId);
+        if (mso == null) {
+            notFound(resp, "Mso Not Found");
+            return null;
+        }
+        
+        // channels
+        String channelIdsStr = req.getParameter("channels");
+        if (channelIdsStr == null) {
+            badRequest(resp, MISSING_PARAMETER);
+            return null;
+        }
+        String[] channelIdStrList = channelIdsStr.split(",");
+        List<Long> channelIdList = new ArrayList<Long>();
+        Long channelId = null;
+        for (String channelIdStr : channelIdStrList) {
+            
+            channelId = null;
+            try {
+                channelId = Long.valueOf(channelIdStr);
+            } catch(Exception e) {
+            }
+            if (channelId != null) {
+                channelIdList.add(channelId);
+            }
+        }
+        // use channelIdList to do something
+        
+        okResponse(resp);
+        return null;
+    }
+    
+    @RequestMapping(value = "msos/{msoId}/store", method = RequestMethod.DELETE)
+    public @ResponseBody
+    String storeChannelRemove(HttpServletRequest req,
+            HttpServletResponse resp, @PathVariable("msoId") String msoIdStr) {
+        
+        Long msoId = null;
+        try {
+            msoId = Long.valueOf(msoIdStr);
+        } catch (NumberFormatException e) {
+        }
+        if (msoId == null) {
+            notFound(resp, INVALID_PATH_PARAMETER);
+            return null;
+        }
+        
+        Mso mso = msoMngr.findById(msoId);
+        if (mso == null) {
+            notFound(resp, "Mso Not Found");
+            return null;
+        }
+        
+        // channels
+        String channelIdsStr = req.getParameter("channels");
+        if (channelIdsStr == null) {
+            badRequest(resp, MISSING_PARAMETER);
+            return null;
+        }
+        String[] channelIdStrList = channelIdsStr.split(",");
+        List<Long> channelIdList = new ArrayList<Long>();
+        Long channelId = null;
+        for (String channelIdStr : channelIdStrList) {
+            
+            channelId = null;
+            try {
+                channelId = Long.valueOf(channelIdStr);
+            } catch(Exception e) {
+            }
+            if (channelId != null) {
+                channelIdList.add(channelId);
+            }
+        }
+        // use channelIdList to do something
+        
+        okResponse(resp);
+        return null;
+    }
+
+}
