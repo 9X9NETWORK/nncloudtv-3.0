7960011177bbfea2e80c7fc5a032dc72cdec5465
barton <barton@a0a5f996-2bb4-401c-8778-395c3c01700f>
2013-04-17 08:33:26 +0000

    fix
    git-svn-id: https://everest.teltel.com:8443/repos/misc/9x9dev/trunk/nncloudtv@4517 a0a5f996-2bb4-401c-8778-395c3c01700f

--- src/main/java/com/nncloudtv/service/SysTagMapManager.java
+++ src/main/java/com/nncloudtv/service/SysTagMapManager.java
@@ -171,8 +171,8 @@ public class SysTagMapManager {
         for (SysTagMap item : sysTagMaps) {
             result = channelMap.get(item.getChannelId());
             if (result != null) {
-                result.setTimeStart(item.getTimeStart());
-                result.setTimeEnd(item.getTimeEnd());
+                //result.setTimeStart(item.getTimeStart());
+                //result.setTimeEnd(item.getTimeEnd());
                 result.setSeq(item.getSeq());
                 results.add(result);
             }
