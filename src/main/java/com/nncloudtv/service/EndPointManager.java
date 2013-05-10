d74a47afca5d8b638a351b43b0fa29ee184f82b5
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2013-05-10 19:34:21 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/9x9dev/trunk/nncloudtv@4689 a0a5f996-2bb4-401c-8778-395c3c01700f

---
+++ src/main/java/com/nncloudtv/service/EndPointManager.java
@@ -0,0 +1,48 @@
+package com.nncloudtv.service;
+
+import java.util.Date;
+
+import com.nncloudtv.dao.EndPointDao;
+import com.nncloudtv.model.EndPoint;
+
+public class EndPointManager {
+
+    EndPointDao dao = new EndPointDao();
+    
+    public EndPoint save(EndPoint endpoint) {        
+        if (endpoint == null) {
+            return null;
+        }
+        Date now = new Date();
+        endpoint.setUpdateDate(now);
+        if (endpoint.getCreateDate() == null) {
+            endpoint.setCreateDate(now);
+        }
+        endpoint = dao.save(endpoint);        
+        return endpoint;
+    }
+
+    public short getVendorType(String vendor) {
+        if (vendor == null) 
+            return EndPoint.VENDOR_UNDEFINED;
+        vendor = vendor.toLowerCase();
+        if (vendor.equals("gcm"))
+            return EndPoint.VENDOR_GCM;
+        if (vendor.equals("apns"))
+            return EndPoint.VENDOR_APNS;
+        if (vendor.equals("sms"))
+            return EndPoint.VENDOR_SMS;
+        return EndPoint.VENDOR_UNDEFINED;
+    }
+    
+    public EndPoint findByEndPoint(long userId, long msoId, String token, short vendor) {
+        if (vendor == 0)
+            return null;
+        return dao.findByEndPoint(userId, msoId, token, vendor);                
+    }
+    
+    public void delete(EndPoint endpoint) {
+        dao.delete(endpoint);
+    }        
+    
+}
