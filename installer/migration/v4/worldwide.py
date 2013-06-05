3aba84c2d9e5f8915fb50bb6934fc0956e0b5fdb
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2013-06-05 22:13:47 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/9x9dev/trunk/nncloudtv@4830 a0a5f996-2bb4-401c-8778-395c3c01700f

--- installer/migration/v4/worldwide.py
+++ installer/migration/v4/worldwide.py
@@ -14,53 +14,57 @@ dbcontent = MySQLdb.connect (host = "localhost",
                              use_unicode = True,
                              db = "nncloudtv_content")
 
-feed = open("data/worldwide.csv", "rU")
-i = 0
-cntNnChannel=0
-cursor = dbcontent.cursor()
-for line in feed:
-  # -- get data
-  data = line.split(',')
-  cId = data[0]
-  name = data[1]
-  url = data[2]
-  category = data[3]
-  lang = data[5]
-  sphere = data[6]
-  print "cid: " + cId + "; name:" + name + "; url:" + url + "; category:" + category + "; lang:" + lang + "; sphere:" + sphere
-  # -- translate data
-  sphere = translate.get_sphere(sphere)
-  lang = translate.get_lang(lang)
-  if cId == "":
-    url = url.strip()
-    posturl = "http://localhost:8080/wd/urlSubmit?url=" + url + "&lang=" + lang + "&sphere=" + sphere
-    print "lookup cid posturl:" + posturl
-    cId = urllib2.urlopen(posturl).read()
-    print "lookup id:" + cId
-  if url == "":
-    cntNnChannel = cntNnChannel+1
-  else:
-    if not "error" in cId:
-       systagId = translate.get_systagId(category)
-       print "systagId:" + str(systagId)
-       if systagId == -1:
-          break
-       print "updated lang:" + lang + ";updated sphere:" + sphere
-       cursor.execute("""
-          update nnchannel set lang=%s, sphere=%s, isPublic=true, status=0 where id=%s
-          """, (lang, sphere, cId))
-       try:
-          cursor.execute("""
-             insert into systag_map (systagId, channelId, createDate, updateDate) values (%s, %s, now(), now())
-           """, (systagId, cId))
-       except MySQLdb.IntegrityError:
-          print "duplicate key"
-  i = i+1
-  print "----------"
-  #if i > 2:
-  #   break
+files = ['worldwide', 'us']
+for myfile in files:
+  filename = "data/" + myfile + ".csv"
+  print filename
+  feed = open(filename, "rU")
+  i = 0
+  cntNnChannel=0
+  cursor = dbcontent.cursor()
+  for line in feed:
+    # -- get data
+    data = line.split(',')
+    cId = data[0]
+    name = data[1]
+    url = data[2]
+    category = data[3]
+    lang = data[5]
+    sphere = data[6]
+    print "cid: " + cId + "; name:" + name + "; url:" + url + "; category:" + category + "; lang:" + lang + "; sphere:" + sphere
+    # -- translate data
+    sphere = translate.get_sphere(sphere)
+    lang = translate.get_lang(lang)
+    if cId == "":
+      url = url.strip()
+      posturl = "http://localhost:8080/wd/urlSubmit?url=" + url + "&lang=" + lang + "&sphere=" + sphere
+      print "lookup cid posturl:" + posturl
+      cId = urllib2.urlopen(posturl).read()
+      print "lookup id:" + cId
+    if url == "":
+      cntNnChannel = cntNnChannel+1
+    else:
+      if not "error" in cId:
+         systagId = translate.get_systagId(category)
+         print "systagId:" + str(systagId)
+         if systagId == -1:
+            break
+         print "updated lang:" + lang + ";updated sphere:" + sphere
+         cursor.execute("""
+            update nnchannel set lang=%s, sphere=%s, isPublic=true, status=0 where id=%s
+            """, (lang, sphere, cId))
+         try:
+            cursor.execute("""
+               insert into systag_map (systagId, channelId, createDate, updateDate) values (%s, %s, now(), now())
+             """, (systagId, cId))
+         except MySQLdb.IntegrityError:
+            print "duplicate key"
+    i = i+1
+    print "----------"
+    #if i > 2:
+    #   break
  
-dbcontent.commit()  
+#dbcontent.commit()  
 cursor.close ()
 
 print "record done:" + str(i)
