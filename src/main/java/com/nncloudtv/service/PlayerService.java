d88bda6c999c414b59172fc419cee456f1eccbfa
louis <louis@a0a5f996-2bb4-401c-8778-395c3c01700f>
2013-06-27 04:05:36 +0000

    remove root prefix
    git-svn-id: https://everest.teltel.com:8443/repos/misc/9x9dev/trunk/nncloudtv@4980 a0a5f996-2bb4-401c-8778-395c3c01700f

--- src/main/java/com/nncloudtv/service/PlayerService.java
+++ src/main/java/com/nncloudtv/service/PlayerService.java
@@ -199,7 +199,7 @@ public class PlayerService {
     // http://player.9x9.tv/tv#/promotion/{ch}/{ep}
     public String getPlayerProtionUrl(HttpServletRequest req, Mso mso, String ch, String ep) {
         
-        String root = NnNetUtil.getUrlRoot(req).replaceFirst("^www\\.", "");
+        String root = NnNetUtil.getUrlRoot(req).replaceFirst("^http(s)?:\\/\\/(www\\.)?", "");
         
         if (MsoManager.isNNMso(mso)) {
             root = "player." + root;
