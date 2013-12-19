470f37b8ba811a2f4b3119c158b892525acb3171
louis <louis@a0a5f996-2bb4-401c-8778-395c3c01700f>
2013-12-19 05:23:22 +0000

    installer.sh
    git-svn-id: https://everest.teltel.com:8443/repos/misc/9x9dev/trunk/nncloudtv@5823 a0a5f996-2bb4-401c-8778-395c3c01700f

--- installer/installer.sh
+++ installer/installer.sh
@@ -8,11 +8,11 @@ script_dir=`dirname "$script_path"`
 cd "$script_dir"
 
 cd ..
-mvn \
+mvn -DskipTests \
     clean \
     compile \
     datanucleus:enhance \
     install war:war \
 && sudo cp target/root.war /usr/share/jetty/webapps/root.war \
-&& sudo service jetty restart; sudo service memcached restart
+&& (sudo service jetty restart; sudo service memcached restart)
 
