f72b0eb50f274a339ee025bd9ff0f0d111c5b1e8
louis <louis@a0a5f996-2bb4-401c-8778-395c3c01700f>
2013-12-19 05:22:45 +0000

    update installer.sh
    destroy nncms
    
    git-svn-id: https://everest.teltel.com:8443/repos/misc/9x9dev/trunk/nncloudtv@5812 a0a5f996-2bb4-401c-8778-395c3c01700f

--- installer/installer.sh
+++ installer/installer.sh
@@ -1,4 +1,5 @@
-o "install"
+clear
+echo "install"
 sudo service jetty stop
 sudo service memcached stop
 cd nncloudtv/
@@ -6,10 +7,6 @@ mvn clean compile
 mvn datanucleus:enhance
 mvn war:war
 cd ../
-cd nncms/
-mvn clean compile war:war
-cd ../
 sudo cp nncloudtv/target/root.war /usr/share/jetty/webapps/root.war
-sudo cp nncms/target/cms.war /usr/share/jetty/webapps/cms.war
 sudo service jetty start
 sudo service memcached start
