9666bd98f39d3ffbb2e68017b62272d449cc39d9
louis <louis@a0a5f996-2bb4-401c-8778-395c3c01700f>
2013-12-19 05:23:12 +0000

    installer.sh
    git-svn-id: https://everest.teltel.com:8443/repos/misc/9x9dev/trunk/nncloudtv@5820 a0a5f996-2bb4-401c-8778-395c3c01700f

--- installer/installer.sh
+++ installer/installer.sh
@@ -1,12 +1,20 @@
-clear
-echo "install"
+#!/bin/sh
+#
+# installer.sh - developement installer script
+#
+
+script_path=`readlink -f "$0"`
+script_dir=`dirname "$script_path"`
+cd "$script_dir"
+
+cd ..
 sudo service jetty stop
 sudo service memcached stop
-cd nncloudtv/
 mvn clean compile
 mvn datanucleus:enhance
+mvn install
 mvn war:war
-cd ../
-sudo cp nncloudtv/target/root.war /usr/share/jetty/webapps/root.war
+sudo cp target/root.war /usr/share/jetty/webapps/root.war
 sudo service jetty start
 sudo service memcached start
+
