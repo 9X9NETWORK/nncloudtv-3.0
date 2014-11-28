172a409b2d258040d4353ad64ef28e5bbd22b377
Louis Jeng <louisje@gmail.com>
2014-11-28 17:13:49 +0800

    fix installer.sh

--- installer/installer.sh
+++ installer/installer.sh
@@ -16,26 +16,13 @@ echo " **    9x9 DevOps Installer  **"
 echo "   **                          *"
 echo "     *****************************"
 echo
-test "$1" != "-n" \
-    && echo -n "Checking your sudo permission ... " \
-    && sudo id \
-    && if test $? -eq 1; then echo "failed."; exit; fi \
-    && echo
 
 cd ..
-
-if test "$1" = "-x"; then # speed up
-    mvn -Dmaven.test.skip=true compile war:war \
-    && sudo cp -v target/root.war /usr/share/$jetty/webapps/root.war \
-    && sudo service $jetty restart
-    exit
-fi
 
 mvn -Dmaven.test.skip=true clean\
     compile \
     datanucleus:enhance \
     install war:war \
 && test "$1" != "-n" \
-&& sudo cp -v target/root.war /usr/share/$jetty/webapps/root.war \
-&& (sudo service $jetty restart; sudo service memcached restart)
+&& cp -v target/nncloudtv-0.0.1-SNAPSHOT.war target/root.war
 
